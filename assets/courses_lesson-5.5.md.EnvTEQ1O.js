58af7ae0f3afb35315b53090d412e91c34b49a0d
yujinpan <yujinpan@users.noreply.github.com>
2024-09-27 04:00:36 +0000

    deploy: 672635d5ac5df4c6162d827ec5462f387aba5dc8

---
+++ assets/courses_lesson-5.5.md.EnvTEQ1O.js
@@ -0,0 +1,85 @@
+import{D as k,c as h,I as o,j as t,a as i,o as r}from"./chunks/framework.CIJOd5-K.js";const a=t("h1",{id:"lesson-5-5",tabindex:"-1"},[i("Lesson-5.5 "),t("a",{class:"header-anchor",href:"#lesson-5-5","aria-label":'Permalink to "Lesson-5.5"'},"​")],-1),z=t("p",null,"你好，我是星荣。 欢迎来到零基础学英语的第5.5课。 这节课是第一课到第五课的复习总结。以后我们的课程安排就会像这样，整数 课是新课，学习新内容，非整数的就是复习课。 这节课的复习分为两个部分。第一个部分是类似我们常规的连词成句训练。第 二部分是关于英语发音知识的学习。我们先来系统的回顾一下前五课的学习内容。 我们学习的第一个框架，第一人称和第二人称所对应的四个表达想法的动词都 是使用它们的原型。当人称变成第三人称单数，那么对应的四个动词要发生相应的 变化来和人称保持一致。后面添加动词小组合以及时间词和地点词就可以了。 接下来的难点就是这个框架的否定形式。 第一人称和第二人称否定时，直接在人称后面添加do not 简称don't就可以 了；第三人称单数否定时需要在人称后面添加does not 简称doesn't，但这里需要 注意把后面的动词都恢复原型，因为这里已经有doesn't这个否定词和人称保持一 致了，不需要动词发生变化，用原型即可。然后后面还是一样连接我们熟悉的动作 小组合以及时间地点词就好了。 第二个框架 表示的含义可以理解为它是，先用一个代词it开头，用is连接后 再添加描述词，然后再用我们学过的动作小组合+时间词地点词来解释这个it所代 指的内容。 当然，如果你不想展开解释这个代词it具体指的是什么也可以。 如果我们把it这个代词换成其它的代词，换成男他女她你我，那么中间的这个 连接词要根据不同的人称变换形式，I的时候用am，you的时候用are，三种形式 的TA用is。 所以这三个词只是形式不同，含义是没有区别的，他们全都是be的变体，原型 是be。你可以理解为把be右边的词的属性赋予给be左边的词。 我们又添加了其他可以赋予含义的词比如地点词here, 年龄18岁，姓名。看例 句，i am important。 我是重要的。是把重要的这个词的属性赋予给我。You are here。你在这里，是把here这个表示地点的词的属性赋予给you。He is 18。他18 岁，把18岁这个年龄属性赋予给he。 根据第一个框架，我们又添加了三个新的表示动作的词。表示有能力，可以can； 表示应该 should；表示将会wil。这三个词不管在什么人称后面都不会发生形式上 的变化，但是只有一个要求，就是后面直接添加动词的原型，去掉to。这三个词的 否定形式也非常简单，直接再他们后面添加一个not就可以。 这就是我们第一到第五课学习的内容，我们把这几个框架里学到的东西随意搭 配组合，把一个单词多次放到不同的语境中去运用，很快你就可以牢牢地掌握住它 们。 祝好运！ 星荣",-1),s=t("p",null,"微信：xingrong-english 公众号：Hi要大声说出来",-1),w=JSON.parse('{"title":"Lesson-5.5","description":"","frontmatter":{},"headers":[],"relativePath":"courses/lesson-5.5.md","filePath":"courses/lesson-5.5.md","lastUpdated":null}'),l={name:"courses/lesson-5.5.md"},v=Object.assign(l,{setup(d){const n=[{zh:"我",en:"I",kk:"/aɪ/"},{zh:"需要",en:"need",kk:"/nid/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"我需要做这件事",en:"I need to do it",kk:"/aɪ/ /nid/ /tə/ /du/ /ɪt/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"我需要现在做这件事",en:"I need to do it now",kk:"/aɪ/ /nid/ /tə/ /du/ /ɪt/ /naʊ/"},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"它是重要的",en:"it is important",kk:"/ɪt/ /ɪz/ /ɪm'pɔrtnt/"},{zh:"非常",en:"very",kk:"/ˈvɛri/"},{zh:"它是非常重要的",en:"it is very important",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"因为它是非常重要的",en:"because it is very important",kk:"/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/"},{zh:`我需要现在做这件事 \r
+因为它是非常重要的`,en:`I need to do it now  \r
+because it is very important`,kk:`/aɪ/ /nid/ /tə/ /du/ /ɪt/ /naʊ/  \r
+/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/`},{zh:"我",en:"me",kk:"/mi/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"我必须做这件事",en:"I have to do it",kk:"/aɪ/ /hæv/ /tə/ /du/ /ɪt/"},{zh:"每天",en:"every day",kk:"/'ɛvri/ /de/"},{zh:"我必须每天做这件事",en:"I have to do it every day",kk:"/aɪ/ /hæv/ /tə/ /du/ /ɪt/ /'ɛvri/ /de/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`它是非常重要的对我来说 \r
+所以我必须每天做这件事`,en:`It is very important for me  \r
+so I have to do it every day`,kk:`/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/  \r
+/so/ /aɪ/ /hæv/ /tə/ /du/ /ɪt/ /'ɛvri/ /de/`},{zh:"不",en:"not",kk:"/nɑt/"},{zh:"它是不重要的",en:"it is not important",kk:"/ɪt/ /ɪz/ /nɑt/ /ɪm'pɔrtnt/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:"它是不重要的对我来说",en:"it is not important for me",kk:"/ɪt/ /ɪz/ /nɑt/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"我必须做这件事",en:"I have to do it",kk:"/aɪ/ /hæv/ /tə/ /du/ /ɪt/"},{zh:"我不必做这件事",en:"I don't have to do it",kk:"/aɪ/ /dont/ /hæv/ /tə/ /du/ /ɪt/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"我不必现在做这件事",en:"I don't have to do it now",kk:"/aɪ/ /dont/ /hæv/ /tə/ /du/ /ɪt/ /naʊ/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`它是不重要的对我来说 \r
+所以我不必现在做这件事`,en:`It is not important for me  \r
+so I don't have to do it now`,kk:`/ɪt/ /ɪz/ /nɑt/ /ɪm'pɔrtnt/ /fɝ/ /mi/  \r
+/so/ /aɪ/ /dont/ /hæv/ /tə/ /du/ /ɪt/ /naʊ/`},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"我想要知道",en:"I want to know",kk:"/aɪ/ /wɑnt/ /tə/ /no/"},{zh:"可能的",en:"possible",kk:"/'pɑsəbl/"},{zh:"它是可能的",en:"it is possible",kk:"/ɪt/ /ɪz/ /'pɑsəbl/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"做这件事（它）是可能的",en:"it is possible to do it",kk:"/ɪt/ /ɪz/ /'pɑsəbl/ /tə/ /du/ /ɪt/"},{zh:"今天",en:"today",kk:"/tə'de/"},{zh:"今天做这件事（它）是可能的",en:"it is possible to do it today",kk:"/ɪt/ /ɪz/ /'pɑsəbl/ /tə/ /du/ /ɪt/ /tə'de/"},{zh:"是否",en:"if",kk:"/ɪf/"},{zh:`我想要知道今天做这件事是否\r
+（它）是可能的`,en:`I want to know if it is possible to \r
+do it today`,kk:`/aɪ/ /wɑnt/ /tə/ /no/ /ɪf/ /ɪt/ /ɪz/ /'pɑsəbl/ /tə/ \r
+/du/ /ɪt/ /tə'de/`},{zh:"我",en:"me",kk:"/mi/"},{zh:"说话",en:"to talk",kk:"/tə/ /tɔk/"},{zh:"和我",en:"with me",kk:"/wɪð/ /mi/"},{zh:"和我说话",en:"to talk with me",kk:"/tə/ /tɔk/ /wɪð/ /mi/"},{zh:"你可以",en:"you can",kk:"/ju/ /kæn/"},{zh:"你可以和我说话",en:"you can talk with me",kk:"/ju/ /kæn/ /tɔk/ /wɪð/ /mi/"},{zh:"我想要知道",en:"I want to know",kk:"/aɪ/ /wɑnt/ /tə/ /no/"},{zh:"是否",en:"if",kk:"/ɪf/"},{zh:`我想要知道是否你可以和我说\r
+话`,en:`I want to know if you can talk with \r
+me`,kk:`/aɪ/ /wɑnt/ /tə/ /no/ /ɪf/ /ju/ /kæn/ /tɔk/ /wɪð/ \r
+/mi/`},{zh:"今晚",en:"tonight",kk:"/tə'naɪt/"},{zh:`我想要知道是否你今晚可以和\r
+我说话`,en:`I want to know if you can talk with \r
+me tonight`,kk:`/aɪ/ /wɑnt/ /tə/ /no/ /ɪf/ /ju/ /kæn/ /tɔk/ /wɪð/ \r
+/mi/  /tə'naɪt/`},{zh:"它是重要的",en:"it is important",kk:"/ɪt/ /ɪz/ /ɪm'pɔrtnt/"},{zh:"非常",en:"very",kk:"/ˈvɛri/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`我想要知道是否你今晚可以和\r
+我说话因为它是非常重要的对\r
+我来说`,en:`I want to know if you can talk with \r
+me  tonight  because  it  is  very \r
+important for me`,kk:`/aɪ/ /wɑnt/ /tə/ /no/ /ɪf/ /ju/ /kæn/ /tɔk/ /wɪð/ \r
+/mi/  /tə'naɪt/    /bɪ'kɔz/  /ɪt/  /ɪz/  /ˈvɛri/ \r
+/ɪm'pɔrtnt/ /fɝ/ /mi/`},{zh:"你",en:"you",kk:"/ju/"},{zh:"告诉你",en:"to tell you",kk:"/tə/ /tɛl/ /ju/"},{zh:"我必须告诉你",en:"I have to tell you",kk:"/aɪ/ /hæv/ /tə/ /tɛl/ /ju/"},{zh:"某件事情",en:"something",kk:"/'sʌmθɪŋ/"},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"重要的某件事情",en:"something important",kk:"/'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"我必须告诉你重要的某件事情",en:`I  have  to  tell you  something \r
+important`,kk:"/aɪ/ /hæv/ /tə/ /tɛl/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:`我必须现在告诉你重要的某件\r
+事情`,en:`I  have  to  tell  you  something \r
+important now`,kk:`/aɪ/ /hæv/ /tə/ /tɛl/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/ \r
+/naʊ/`},{zh:"紧急的",en:"urgent",kk:"/'ɝdʒənt/"},{zh:"它是紧急的",en:"it is urgent",kk:"/ɪt/ /ɪz/ /'ɝdʒənt/"},{zh:"它是非常紧急的",en:"it is very urgent",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /'ɝdʒənt/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"我必须现在告诉你重要的某件I",en:"have  to  tell  you  something",kk:"/aɪ/ /hæv/ /tə/ /tɛl/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"事情因为它是非常紧急的",en:`important now because it is very \r
+urgent`,kk:"/naʊ/ /bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /'ɝdʒənt/"},{zh:"这个食物",en:"the food",kk:"/ðə/ /fud/"},{zh:"我喜欢",en:"I like",kk:"/aɪ/ /laɪk/"},{zh:"我喜欢这个食物",en:"I like the food",kk:"/aɪ/ /laɪk/  /ðə/ /fud/"},{zh:"我不喜欢这个食物",en:"I don't like the food",kk:"/aɪ/ /dont/ /laɪk/  /ðə/ /fud/"},{zh:"我想要",en:"I want",kk:"/aɪ/ /wɑnt/"},{zh:"在这里",en:"to be here",kk:"/tə/ /bi/ /hɪr/"},{zh:"我想要在这里",en:"I want to be here",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /hɪr/"},{zh:"我不想在这里",en:"I don't want to be here",kk:"/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /hɪr/"},{zh:"一整天",en:"all the day",kk:"/ɔl/ /ðə/ /de/"},{zh:"我不想一整天在这里",en:"I don't want to be here all the day",kk:"/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /hɪr/ /ɔl/ /ðə/ /de/"},{zh:"而且",en:"and",kk:"/ənd/"},{zh:`我不喜欢这个食物而且 \r
+我不想一整天在这里`,en:`I don't like the food and  \r
+I don't want to be here all the day`,kk:`/aɪ/ /dont/ /laɪk/ /ðə/ /fud/ /ənd/  \r
+/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /hɪr/ /ɔl/ /ðə/ /de/`},{zh:"帮助",en:"to help",kk:"/tə/ /hɛlp/"},{zh:"帮助我",en:"to help me",kk:"/tə/ /hɛlp/ /mi/"},{zh:"你必须帮助我",en:"you have to help me",kk:"/ju/ /hæv/ /tə/ /hɛlp/ /mi/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"你必须现在帮助我",en:"you have to help me now",kk:"/ju/ /hæv/ /tə/ /hɛlp/ /mi/ /naʊ/"},{zh:"非常重要",en:"very important",kk:"/ˈvɛri/ /ɪm'pɔrtnt/"},{zh:"它是非常重要的",en:"it is very important",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`它是非常重要的对我来说 \r
+所以你必须现在帮助我`,en:`It is very important for me  \r
+so you have to help me now`,kk:`/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/  \r
+/so/ /ju/ /hæv/ /tə/ /hɛlp/ /mi/ /naʊ/`},{zh:"做这件事",en:"to do it t",kk:"ə/ /du/ /ɪt/"},{zh:"我必须做这件事",en:"I have to do it",kk:"/aɪ/ /hæv/ /tə/ /du/ /ɪt/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"我必须现在做这件事",en:"I have to do it now",kk:"/aɪ/ /hæv/ /tə/ /du/ /ɪt/ /naʊ/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`它是非常重要的对我来说 \r
+所以我必须现在做这件事`,en:`It is very important for me  \r
+so I have to do it now`,kk:`/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/  \r
+/so/ /aɪ/ /hæv/ /tə/ /du/ /ɪt/ /naʊ/`},{zh:"告诉我",en:"to tell me",kk:"/tə/ /tɛl/ /mi/"},{zh:"你需要告诉我",en:"you need to tell me",kk:"/ju/ /nid/ /tə/ /tɛl/ /mi/"},{zh:"事实",en:"the truth",kk:"/ðə/ /trʊθ/"},{zh:"你需要告诉我事实",en:"you need to tell me the truth",kk:"/ju/ /nid/ /tə/ /tɛl/ /mi/ /ðə/ /trʊθ/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`你需要告诉我事实 \r
+因为它是非常重要的对我来说`,en:`You need to tell me the truth  \r
+because it is very important for me`,kk:`/ju/ /nid/ /tə/ /tɛl/ /mi/ /ðə/ /trʊθ/  \r
+/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/`},{zh:"解释",en:"to explain",kk:"/tə/ /ɪk'splen/"},{zh:"原因",en:"the reason",kk:"/ðə/ /ˈrizən/"},{zh:"解释原因",en:"to explain the reason",kk:"/tə/ /ɪk'splen/ /ðə/ /ˈrizən/"},{zh:"她想要",en:"she wants",kk:"/ʃi/  /wɑnts/"},{zh:"她想要解释原因",en:"she wants to explain the reason",kk:"/ʃi/  /wɑnts/ /tə/ /ɪk'splen/ /ðə/ /ˈrizən/"},{zh:"吃这个食物",en:"to eat the food",kk:"/tə/ /it/ /ðə/ /fud/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"现在吃这个食物",en:"to eat the food now",kk:"/tə/ /it/ /ðə/ /fud/ /naʊ/"},{zh:"她需要",en:"she needs",kk:"/ʃi/  /nidz/"},{zh:"她需要现在吃这个食物",en:"she needs to eat the food now",kk:"/ʃi/ /nidz/ /tə/ /it/ /ðə/ /fud/ /naʊ/"},{zh:"为什么",en:"why",kk:"/waɪ/"},{zh:`她想要解释原因 \r
+为什么她需要现在吃这个食物`,en:`She wants to explain the reason \r
+why she needs to eat the food now`,kk:`/ʃi/ /wɑnts/ /tə/ /ɪk'splen/ /ðə/ /ˈrizən/  \r
+/waɪ/ /ʃi/ /nidz/ /tə/ /it/ /ðə/ /fud/ /naʊ/`},{zh:"飞翔",en:"to fly",kk:"/tə/ /flaɪ/"},{zh:"天空",en:"sky",kk:"/skaɪ/"},{zh:"在天空中",en:"in the sky",kk:"/ɪn/ /ðə/ /skaɪ/"},{zh:"在天空中飞翔",en:"to fly in the sky",kk:"/tə/ /flaɪ/ /ɪn/ /ðə/ /skaɪ/"},{zh:"这个小鸟",en:"the bird",kk:"/ðə/ /bɝd/"},{zh:"这个小鸟想要",en:"the bird wants",kk:"/ðə/ /bɝd/ /wɑnts/"},{zh:"这个小鸟想要在天空中飞翔",en:"the bird wants to fly in the sky",kk:"/ðə/ /bɝd/ /wɑnts/ /tə/ /flaɪ/ /ɪn/ /ðə/ /skaɪ/"},{zh:"星荣想要",en:"Xingrong wants",kk:"/wɑnts/"},{zh:"星荣想要在天空中飞翔",en:"Xingrong wants to fly in the sky",kk:"/wɑnts/ /tə/ /flaɪ/ /ɪn/ /ðə/ /skaɪ/"},{zh:"但是",en:"but",kk:"/bət/"},{zh:"不可能的",en:"impossible",kk:"/ɪm'pɑsəbl/"},{zh:"它是不可能的",en:"it is impossible",kk:"/ɪt/ /ɪz/ /ɪm'pɑsəbl/"},{zh:`星荣想要再天空中飞翔 \r
+但是这件事是不可能的`,en:`Xingrong wants to fly in the sky  \r
+but it is impossible`,kk:`/wɑnts/ /tə/ /flaɪ/ /ɪn/ /ðə/ /skaɪ/  \r
+/bət/ /ɪt/ /ɪz/ /ɪm'pɑsəbl/`},{zh:"告诉",en:"to tell",kk:"/tə/ /tɛl/"},{zh:"事实",en:"the truth",kk:"/ðə/ /trʊθ/"},{zh:"告诉我事实",en:"to tell me the truth",kk:"/tə/ /tɛl/ /mi/ /ðə/ /trʊθ/"},{zh:"他必须告诉我事实",en:"he has to tell me the truth",kk:"/hi/ /hæz/ /tə/ /tɛl/ /mi/ /ðə/ /trʊθ/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`他必须告诉我事实因为 \r
+它是非常重要的对我来说`,en:`he has to tell me the truth because \r
+it is very important for me`,kk:`/hi/ /hæz/ /tə/ /tɛl/ /mi/ /ðə/ /trʊθ/ /bɪ'kɔz/  \r
+/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/`},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"知道这件事",en:"to know it",kk:"/tə/ /no/ /ɪt/"},{zh:"我需要知道这件事",en:"I need to know it",kk:"/aɪ/ /nid/ /tə/ /no/ /ɪt/"},{zh:"而且",en:"and",kk:"/ənd/"},{zh:`他必须告诉我事实因为它是非\r
+常重要的对我来说而且我需要\r
+知道这件事`,en:`He has to tell me the truth because \r
+it is very important for me and I \r
+need to know it`,kk:`/hi/ /hæz/ /tə/ /tɛl/ /mi/ /ðə/ /trʊθ/ /bɪ'kɔz/ \r
+/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/ /ənd/ /aɪ/ \r
+/nid/ /tə/ /no/ /ɪt/`},{zh:"她想要",en:"she wants",kk:"/ʃi/  /wɑnts/"},{zh:"见到",en:"to see",kk:"/tə/ /si/"},{zh:"她想要见到我",en:"she wants to see me",kk:"/ʃi/  /wɑnts/ /tə/ /si/ /mi/"},{zh:"在这里",en:"to be here",kk:"/tə/ /bi/ /hɪr/"},{zh:"我必须在这里",en:"I have to be here",kk:"/aɪ/ /hæv/ /tə/ /bi/ /hɪr/"},{zh:"我今天必须在这里",en:"I have to be here today",kk:"/aɪ/ /hæv/ /tə/ /bi/ /hɪr/ /tə'de/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`她想要见到我所以 \r
+我今天必须在这里`,en:`She wants to see me so  \r
+I have to be here today`,kk:`/ʃi/  /wɑnts/ /tə/ /si/ /mi/ /so/  \r
+/aɪ/ /hæv/ /tə/ /bi/ /hɪr/ /tə'de/`},{zh:"解释",en:"to explain",kk:"/tə/ /ɪk'splen/"},{zh:"原因",en:"the reason",kk:"/ðə/ /ˈrizən/"},{zh:"解释原因",en:"to explain the reason",kk:"/tə/ /ɪk'splen/  /ðə/ /ˈrizən/"},{zh:"她必须解释原因",en:"she has to explain the reason",kk:"/ʃi/ /hæz/ /tə/ /ɪk'splen/  /ðə/ /ˈrizən/"},{zh:"在家里阅读",en:"to read at home",kk:"/tə/ /rid/ /ət/ /hom/"},{zh:"她喜欢",en:"she likes",kk:"/ʃi/ /laɪks/"},{zh:"她喜欢在家里阅读",en:"she likes to read at home",kk:"/ʃi/ /laɪks/ /tə/ /rid/ /ət/ /hom/"},{zh:"为什么",en:"why",kk:"/waɪ/"},{zh:`她必须解释原因为什么 \r
+她喜欢在家里阅读`,en:`she has to explain the reason why \r
+she likes to read at home`,kk:`/ʃi/ /hæz/ /tə/ /ɪk'splen/  /ðə/ /ˈrizən/ /waɪ/  \r
+/ʃi/ /laɪks/ /tə/ /rid/ /ət/ /hom/`},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"非常",en:"very",kk:"/ˈvɛri/"},{zh:"它是非常重要的",en:"it is very important",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`她必须解释原因为什么她喜欢\r
+在家里阅读因为这件事是非常\r
+重要的`,en:`She has to explain the reason why \r
+she likes to read at home because it \r
+is very important`,kk:`/ʃi/ /hæz/ /tə/ /ɪk'splen/ /ðə/ /ˈrizən/ /waɪ/ \r
+/ʃi/ /laɪks/  /tə/ /rid/ /ət/ /hom/  /bɪ'kɔz/ \r
+/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/`},{zh:"他想要",en:"he wants",kk:"/hi/ /wɑnts/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"他想要做这件事",en:"he wants to do it",kk:"/hi/ /wɑnts/ /tə/ /du/ /ɪt/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"他想要现在做这件事",en:"he wants to do it now",kk:"/hi/ /wɑnts/ /tə/ /du/ /ɪt/ /naʊ/"},{zh:"他不想现在做这件事",en:"he doesn't want to do it now hi",kk:"/ /ˈdʌznt/ /wɑnt/ /tə/ /du/ /ɪt/ /naʊ/"},{zh:"紧急的",en:"urgent",kk:"/'ɝdʒənt/"},{zh:"它是紧急的",en:"it is urgent",kk:"/ɪt/ /ɪz/ /'ɝdʒənt/"},{zh:"它是不紧急的",en:"it is not urgent",kk:"/ɪt/ /ɪz//nɑt/ /'ɝdʒənt/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`他不想现在做这件事 \r
+因为它是不紧急的`,en:`He doesn't want to do it now \r
+because it is not urgent \r
+hi`,kk:`/ /ˈdʌznt/ /wɑnt/ /tə/ /du/ /ɪt/ /naʊ/  \r
+/bɪ'kɔz/ /ɪt/ /ɪz//nɑt/ /'ɝdʒənt/`},{zh:"可以",en:"can",kk:"/kæn/"},{zh:"我可以",en:"I can",kk:"/aɪ/ /kæn/"},{zh:"做这件事",en:"to do it",kk:"/tə/ /du/ /ɪt/"},{zh:"我可以做这件事",en:"I can do it",kk:"/aɪ/ /kæn/ /du/ /ɪt/"},{zh:"单独地",en:"alone",kk:"/ə'lon/"},{zh:"我可以单独做这件事",en:"I can do it alone",kk:"/aɪ/ /kæn/ /du/ /ɪt/ /ə'lon/"},{zh:"我不可以单独做这件事",en:"I can not do it alone",kk:"/aɪ/ /kæn/ /nɑt/ /du/ /ɪt/ /ə'lon/"},{zh:"你必须帮助我",en:"you have to help me",kk:"/ju/ /hæv/ /tə/ /hɛlp/ /mi/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`你必须帮助我 \r
+因为我不可以单独做这件事`,en:`You have to help me  \r
+because I can not do it alone`,kk:`/ju/ /hæv/ /tə/ /hɛlp/ /mi/  \r
+/bɪ'kɔz/ /aɪ/ /kæn/ /nɑt/ /du/ /ɪt/ /ə'lon/`},{zh:"告诉你",en:"tell you",kk:"/tɛl/ /ju/"},{zh:"事实",en:"the truth",kk:"/ðə/ /trʊθ/"},{zh:"告诉你事实",en:"tell you the truth",kk:"/tɛl/ /ju/ /ðə/ /trʊθ/"},{zh:"我将会告诉你事实",en:"I will tell you the truth",kk:"/aɪ/ /wɪl/ /tɛl/ /ju/ /ðə/ /trʊθ/"},{zh:"我将不会告诉你事实",en:"I will not tell you the truth",kk:"/aɪ/ /wɪl/ /nɑt/ /tɛl/ /ju/ /ðə/ /trʊθ/"},{zh:"未来",en:"the future",kk:"/ðə/ /'fjʊtʃɚ/"},{zh:"在未来",en:"in the future",kk:"/ɪn/ /ðə/ /'fjʊtʃɚ/"},{zh:"我将会在未来告诉你事实",en:"I will tell you the truth in the future",kk:`/aɪ/ /wɪl/  /tɛl/  /ju/  /ðə/ /trʊθ/  /ɪn/  /ðə/ \r
+/'fjʊtʃɚ/`},{zh:"我将不会在未来告诉你事实",en:`I will not tell you the truth in the \r
+future`,kk:`/aɪ/ /wɪl/ /nɑt/ /tɛl/ /ju/ /ðə/ /trʊθ/ /ɪn/ /ðə/ \r
+/'fjʊtʃɚ/`},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"它是不重要的",en:"it is not important",kk:"/ɪt/ /ɪz/ /nɑt/ /ɪm'pɔrtnt/"},{zh:"所以",en:"so",kk:"/so/"},{zh:`它是不重要的所以 \r
+我将会在未来告诉你事实`,en:`it is not important so  \r
+I will tell you the truth in the future`,kk:`/ɪt/ /ɪz/ /nɑt/ /ɪm'pɔrtnt/ /so/ /aɪ/ /wɪl/ /tɛl/ \r
+/ju/ /ðə/ /trʊθ/ /ɪn/ /ðə/ /'fjʊtʃɚ`},{zh:"她可以",en:"she can",kk:"/ʃi/ /kæn/"},{zh:"解释",en:"explain",kk:"/ɪk'splen/"},{zh:"原因",en:"the reason",kk:"/ðə/ /ˈrizən/"},{zh:"解释原因",en:"explain the reason",kk:"/ɪk'splen/ /ðə/ /ˈrizən/"},{zh:"她可以解释原因",en:"she can explain the reason",kk:"/ʃi/ /kæn/ /ɪk'splen/ /ðə/ /ˈrizən/"},{zh:"她不可以解释原因",en:"she can not explain the reason",kk:"/ʃi/ /kæn/ /nɑt/ /ɪk'splen/ /ðə/ /ˈrizən/"},{zh:"为什么",en:"why",kk:"/waɪ/"},{zh:"他今晚不可以和我说话",en:"he can not talk with me tonight",kk:"/hi/ /kæn/ /nɑt/ /tɔk/ /wɪð/ /mi/ /tə'naɪt/"},{zh:`她可以解释原因为什么 \r
+他今晚不可以和我说话`,en:`She can explain the reason why  \r
+he can not talk with me tonight`,kk:`/ʃi/ /kæn/ /nɑt/ /ɪk'splen/ /ðə/ /ˈrizən/ /waɪ/ \r
+/hi/ /kæn/ /nɑt/ /tɔk/ /wɪð/ /mi/ /tə'naɪt/`}];return(m,u)=>{const e=k("StatementGroup");return r(),h("div",null,[a,z,s,o(e,{data:n})])}}});export{w as __pageData,v as default};
