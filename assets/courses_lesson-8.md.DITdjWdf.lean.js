01b1e4fc93c051284827eef2673a34bcc66cae96
yujinpan <yujinpan@users.noreply.github.com>
2024-10-16 07:53:18 +0000

    deploy: 36a8004aa4fc7ab7fd44e1527ecb5c72f5883fdc

---
+++ assets/courses_lesson-8.md.DITdjWdf.lean.js
@@ -0,0 +1,108 @@
+import{D as o,c as k,I as h,j as n,a as r,o as i}from"./chunks/framework.CIJOd5-K.js";const a=n("h1",{id:"lesson-8",tabindex:"-1"},[r("Lesson-8 "),n("a",{class:"header-anchor",href:"#lesson-8","aria-label":'Permalink to "Lesson-8"'},"​")],-1),u=n("p",null,"你好，我是星荣。 欢迎来到零基础学英语的第八课. 上节课我们添加了两个表达想法的词，第一个表示打算、计划，plan，第二个 表示愿意，想要，would like，这两个词在后面连接动作词的时候都要先加上to。 第一个词plan和之前的四个词的用法一样。主语为第三人称单数时要变成 plans；否定时，第一人称、第二人称和第三人称的复数都需要在它们之前添加don't， 第三人称单数添加doesn't，并需要把动作词恢复原型。 第二个词would like，它的用法比较特殊，你可以理解为是我们学习的第二类 词和第一类词的结合。would属于我们学过第二类表达想法的词，它其实就是will 的过去式，不随人称变化而变化形，且不用添加to直接连接原型的动作词。在这 个词组中，like其实就是would后面连接的动词，所以like一直都是原型，否定时 在would的后面加not；在整个词组would like后面再连接一个动词时需要先添加 to。 第二个知识点其实我们早在第一节课就接触过。用在with、for或者动作词之后 的人称词，和用在句首的人称词是有所不同的。比如我你他以及我你他的复数我们 你们他们，用在句首分别是这样，用在动作词之后或者with、for这些词的后面就 都变成了这样。所以，所有的人称代词里只有你和你们以及非人它的两种形式都是 一样的，其他都是不同的形式。 今天的连词成句训练内容会增加另外一类和人称有关系的词，他们的物主代词 形式，就是我的你的他的我们的你们的他们的，就表示某一个物品的主人，叫做物 主代词。 在开始连词成句之前，给大家介绍一下pdf文档的下载和使用方法，这是非常 重要的复习方法，一定要去下载。 首先需要在微信搜索公众号名字“HI要大声说出来”，关注后回复想要获取的课 程序号，比如这里我们输入7，就会收到自动回复，打开连接后拉到最下面就可以 看到两个下载方式，选择任何一种你觉得方便的进行复制，也欢迎你随手点一下赞 和在看。然后粘贴到浏览器里就可以下载了，下载好之后会是这样的PDF文档。 这些文档的使用方法是这样的。你可以把它们都打印出来，遮挡中文或者英文 部分进行翻译练习。当然这里的翻译练习建议你不要大声读出来，只要看到中文你 的脑海中可以想出答案就可以，因为如果你是初学者的话，有可能会发音错误，大 声朗读了错误的发音就南辕北辙了。 还有一种更重要的使用方法是用来导航。 因为这些课程都是互相有关联的，每节课都会用到前几节课的知识点和单词。 比如说你学到了第七课，发现了有搞不清楚的词语，比如你还是不懂第三人称单数 she后连接的动作词应该如何变换形式，那么你可以找到前面的几节课，通过搜 索”she“来找到之前讲解过的地方再看一遍。比如我们这里通过搜索可以看到第三课 第四课都有大量的”she“这个词，可以返回这两节课来复习。 祝好运！ 星荣",-1),s=n("p",null,"微信：xingrong-english 公众号：Hi要大声说出来",-1),y=JSON.parse('{"title":"Lesson-8","description":"","frontmatter":{},"headers":[],"relativePath":"courses/lesson-8.md","filePath":"courses/lesson-8.md","lastUpdated":null}'),z={name:"courses/lesson-8.md"},j=Object.assign(z,{setup(m){const e=[{zh:"某件事；某个东西",en:"something",kk:"/'sʌmθɪŋ/"},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"重要的某件事",en:"something important",kk:"/'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"告诉",en:"to tell",kk:"/tə/ /tɛl/"},{zh:"告诉你",en:"to tell you",kk:"/tə/ /tɛl/ /ju/"},{zh:"告诉你重要的某件事",en:"to tell you something important",kk:"/tə/ /tɛl/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"我需要告诉你重要的某件事",en:`I  need  to  tell  you  something \r
+important`,kk:"/aɪ/ /nid/ /tə/ /tɛl/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"给",en:"to give",kk:"/tə/ /ɡɪv/"},{zh:"某个东西",en:"something",kk:"/'sʌmθɪŋ/"},{zh:"重要的某个东西",en:"something important",kk:"/'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"我需要给你重要的某个东西",en:`I  need  to  give  you  something \r
+important`,kk:"/aɪ/ /nid/ /tə/ /ɡɪv/ /ju/ /'sʌmθɪŋ/ /ɪm'pɔrtnt/"},{zh:"答案",en:"the answer",kk:"/ði/ /'ænsɚ/"},{zh:"我需要给你答案",en:"I need to give you the answer",kk:"/aɪ/ /nid/ /tə/ /ɡɪv/ /ju/ /ði/ /'ænsɚ/"},{zh:"问题",en:"the question",kk:"/ðə/ /'kwɛstʃən/"},{zh:"问题的答案",en:"the answer to the question",kk:"/ði/ /'ænsɚ/ /tə/ /ðə/ /'kwɛstʃən/"},{zh:"我需要给你问题的答案",en:`I need to give you the answer to the \r
+question`,kk:`/aɪ/ /nid/ /tə/ /ɡɪv/ /ju/ /ði/ /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"我不需要给你问题的答案",en:`I don't need to give you the answer \r
+to the question`,kk:`/aɪ/ /dont/ /nid/ /tə/ /ɡɪv/ /ju/ /ði/ /'ænsɚ/ /tə/ \r
+/ðə/ /'kwɛstʃən/`},{zh:"她",en:"she",kk:"/ʃi/"},{zh:"给",en:"to give",kk:"/tə/ /ɡɪv/"},{zh:"给我们",en:"to give us",kk:"/tə/ /ɡɪv/ /ʌs/"},{zh:"她需要给我们",en:"she needs to give us",kk:"/ʃi/ /nidz/ /tə/ /ɡɪv/ /ʌs/"},{zh:"问题的答案",en:"the answer to the question",kk:"/ði/ /'ænsɚ/ /tə/ /ðə/ /'kwɛstʃən/"},{zh:"她需要给我们问题的答案",en:`she needs to give us the answer to \r
+the question`,kk:`/ʃi/ /nidz/ /tə/ /ɡɪv/ /ʌs/ /ði/ /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"她不需要给我们问题的答案",en:`she doesn't need to give us the \r
+answer to the question`,kk:`/ʃi/ /ˈdʌznt/ /nid/ /tə/ /ɡɪv/ /ʌs/ /ði/ /'ænsɚ/ \r
+/tə/ /ðə/ /'kwɛstʃən/`},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"我想要给你问题的答案",en:`I want to give you the answer to the \r
+question`,kk:`/aɪ/ /wɑnt/ /tə/ /ɡɪv/ /ju/ /ði/ /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"我不想给你问题的答案",en:`I don't want to give you the answer \r
+to the question`,kk:`/aɪ/ /dont/ /wɑnt/ /tə/ /ɡɪv/ /ju/ /ði/ /'ænsɚ/ \r
+/tə/ /ðə/ /'kwɛstʃən/`},{zh:"我想要你给我问题的答案",en:`I want you to give me the answer \r
+to the question`,kk:`/aɪ/ /wɑnt/ /ju/ /tə/ /ɡɪv/ /mi/ /ði/ /'ænsɚ/ /tə/ \r
+/ðə/ /'kwɛstʃən/`},{zh:"我不想要你给我问题的答案",en:`I don't want you to give me the \r
+answer to the question`,kk:`/aɪ/ /dont/ /wɑnt/ /ju/ /tə/ /ɡɪv/ /mi/ /ði/ \r
+/'ænsɚ/ /tə/ /ðə/ /'kwɛstʃən/`},{zh:"一个理由；一个原因",en:"a reason",kk:"/ə/ /ˈrizən/"},{zh:"给我一个理由",en:"to give me a reason",kk:"/tə/ /ɡɪv/ /mi/ /ə/ /ˈrizən/"},{zh:"我需要你给我一个理由",en:"I need you to give me a reason",kk:"/aɪ/ /nid/ /ju/ /tə/ /ɡɪv/ /mi/ /ə/ /ˈrizən/"},{zh:"我不需要你给我一个理由",en:`I don't need you to give me a \r
+reason`,kk:"/aɪ/ /dont/ /nid/ /ju/ /tə/ /ɡɪv/ /mi/ /ə/ /ˈrizən/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"我们应该知道",en:"we should know",kk:"/wi/ /ʃʊd/ /no/"},{zh:"问题的答案",en:"the answer to the question",kk:"/ði/ /'ænsɚ/ /tə/ /ðə/ /'kwɛstʃən/"},{zh:"我们应该知道问题的答案",en:`we should know the answer to the \r
+question`,kk:`/wi/  /ʃʊd/  /no/  /ði/  /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"我们不应该知道问题的答案",en:`we should not know the answer to \r
+the question`,kk:`/wi/ /ʃʊd/ /nɑt/ /no/ /ði/ /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"为什么",en:"why",kk:"/waɪ/"},{zh:`我需要你给我一个理由为什么\r
+我们应该知道问题的答案`,en:`I need you to give me a reason why \r
+we should know the answer to the \r
+question`,kk:`/aɪ/ /nid/ /ju/ /tə/ /ɡɪv/ /mi/ /ə/ /ˈrizən/ /waɪ/ \r
+/wi/  /ʃʊd/  /no/  /ði/  /'ænsɚ/ /tə/ /ðə/ \r
+/'kwɛstʃən/`},{zh:"电话",en:"phone",kk:"/fon/"},{zh:"号码",en:"number",kk:"/'nʌmbɚ/"},{zh:"电话号码",en:"phone number",kk:"/fon/ /'nʌmbɚ/"},{zh:"我的",en:"my",kk:"/maɪ/"},{zh:"我的电话号码",en:"my phone number",kk:"/maɪ/ /fon/ /'nʌmbɚ/"},{zh:"可以",en:"can",kk:"/kæn/"},{zh:"我可以给你",en:"I can give you",kk:"/aɪ/ /kæn/ /ɡɪv/ /ju/"},{zh:"我可以给你我的电话号码",en:"I can give you my phone number",kk:"/aɪ/ /kæn/ /ɡɪv/ /ju/ /maɪ/ /fon/ /'nʌmbɚ/"},{zh:"我不可以给你我的电话号码",en:`I  can  not  give  you  my  phone \r
+number`,kk:`/aɪ/ /kæn/  /nɑt/  /ɡɪv/  /ju/  /maɪ/ /fon/ \r
+/'nʌmbɚ/`},{zh:"你想要",en:"you want",kk:"/ju/ /wɑnt/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"它",en:"it",kk:"/ɪt/"},{zh:"你想要知道它",en:"you want to know it",kk:"/ju//wɑnt/ /tə/ /no/ /ɪt/"},{zh:"如果",en:"if",kk:"/ɪf/"},{zh:`我可以给你我的电话号码如果\r
+你想要知道它`,en:`I can give you my phone number if \r
+you want to know it`,kk:`/aɪ/ /kæn/ /ɡɪv/ /ju/ /maɪ/ /fon/ /'nʌmbɚ/ /ɪf/ \r
+/ju//wɑnt/ /tə/ /no/ /ɪt/`},{zh:"我将会",en:"I will",kk:"/aɪ/ /wɪl/"},{zh:"我将会给你我的电话号码",en:"I will give you my phone number",kk:"/aɪ/ /wɪl/ /ɡɪv/ /ju/ /maɪ/ /fon/ /'nʌmbɚ/"},{zh:"我将不会给你我的电话号码",en:`I  will  not  give  you  my  phone \r
+number`,kk:"/aɪ/ /wɪl/ /nɑt/ /ɡɪv/ /ju/ /maɪ/ /fon/ /'nʌmbɚ/"},{zh:"你不想要",en:"you don't want",kk:"/ju/ /dont/ /wɑnt/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"你不想知道它",en:"you don't want to know it",kk:"/ju/ /dont/ /wɑnt/ /tə/ /no/ /ɪt/"},{zh:`我将不会给你我的电话号码如\r
+果你不想知道它`,en:`I  will  not  give  you  my  phone \r
+number if you don't want to know \r
+it`,kk:`/aɪ/ /wɪl/ /nɑt/ /ɡɪv/ /ju/ /maɪ/ /fon/ /'nʌmbɚ/ \r
+/ɪf/ /ju/ /dont/ /wɑnt/ /tə/ /no/ /ɪt/`},{zh:"你的",en:"your",kk:"/jʊr/"},{zh:"电话号码",en:"phone number",kk:"/fon/ /'nʌmbɚ/"},{zh:"你的电话号码",en:"your phone number",kk:"/jʊr/ /fon/ /'nʌmbɚ/"},{zh:"你应该",en:"you should",kk:"/ju/ /ʃʊd/"},{zh:"你应该给我你的电话号码",en:`you should give me your phone \r
+number`,kk:"/ju/ /ʃʊd/ /ɡɪv/ /mi/ /jʊr/ /fon/ /'nʌmbɚ/"},{zh:"你不应该给我你的电话号码",en:`you should not give me your phone \r
+number`,kk:`/ju/  /ʃʊd/  /nɑt/  /ɡɪv/  /mi/  /jʊr/  /fon/ \r
+/'nʌmbɚ/`},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"我需要知道它",en:"I need to know it",kk:"/aɪ/ /nid/ /tə/ /no/ /ɪt/"},{zh:`你应该给我你的电话号码因为\r
+我需要知道它`,en:`you should give me your phone \r
+number because I need to know it`,kk:`/ju/  /ʃʊd/  /nɑt/  /ɡɪv/  /mi/  /jʊr/  /fon/ \r
+/'nʌmbɚ/ /bɪ'kɔz/ /aɪ/ /nid/ /tə/ /no/ /ɪt/`},{zh:"他的",en:"his",kk:"/hɪz/"},{zh:"电话号码",en:"phone number",kk:"/fon/ /'nʌmbɚ/"},{zh:"他的电话号码",en:"his phone number",kk:"/hɪz/ /fon/ /'nʌmbɚ/"},{zh:"你应该给我他的电话号码",en:`you  should  give  me  his  phone \r
+number`,kk:"/ju/ /ʃʊd/ /ɡɪv/ /mi/ /hɪz/ /fon/ /'nʌmbɚ/"},{zh:"你不应该给我他的电话号码",en:`you should not give me his phone \r
+number`,kk:`/ju/  /ʃʊd/  /nɑt/  /ɡɪv/  /mi/  /hɪz/  /fon/ \r
+/'nʌmbɚ/`},{zh:"你可以给我他的电话号码",en:"you can give me his phone number",kk:"/ju/ /kæn/ /ɡɪv/ /mi/ /hɪz/ /fon/ /'nʌmbɚ/"},{zh:"希望",en:"hope",kk:"/hop/"},{zh:"我希望",en:"I hope",kk:"/aɪ/ /hop/"},{zh:`我希望你可以给我他的电话号\r
+码`,en:`I hope you can give me his phone \r
+number`,kk:`/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /hɪz/ /fon/ \r
+/'nʌmbɚ/`},{zh:"她的",en:"her",kk:"/hɚ/"},{zh:"她的电话号码",en:"her phone number",kk:"/hɚ/ /fon/ /'nʌmbɚ/"},{zh:"你可以给我她的电话号码",en:"you can give me her phone number",kk:"/ju/ /kæn/ /ɡɪv/ /mi/ /hɚ/ /fon/ /'nʌmbɚ/"},{zh:"我希望",en:"I hope",kk:"/aɪ/ /hop/"},{zh:`我希望你可以给我她的电话号\r
+码`,en:`I hope you can give me her phone \r
+number`,kk:`/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /hɚ/ /fon/ \r
+/'nʌmbɚ/`},{zh:"重要的",en:"important",kk:"/ɪm'pɔrtnt/"},{zh:"它是重要的",en:"it is important",kk:"/ɪt/ /ɪz/ /ɪm'pɔrtnt/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:"它是重要的对我来说",en:"it is important for me",kk:"/ɪt/ /ɪz/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"非常",en:"very",kk:"/ˈvɛri/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"因为它是非常重要的对我来说",en:"because it is very important for me",kk:"/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:`我希望你可以给我她的电话号\r
+码因为这件事是非常重要的对\r
+我来说`,en:`I hope you can give me her phone \r
+number  because  it  is  very \r
+important for me`,kk:`/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /hɚ/ /fon/ \r
+/'nʌmbɚ/ /bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ \r
+/fɝ/ /mi/`},{zh:"手",en:"hand",kk:"/hænd/"},{zh:"一只手",en:"a hand",kk:"/ə/ /hænd/"},{zh:"给我一只手（搭把手）",en:"to give me a hand",kk:"/tə/ /ɡɪv/ /mi/ /ə/ /hænd/"},{zh:"你可以给我搭把手",en:"you can give me a hand",kk:"/ju/ /kæn/ /ɡɪv/ /mi/ /ə/ /hænd/"},{zh:"我希望",en:"I hope",kk:"/aɪ/ /hop/"},{zh:"我希望你可以给我搭把手",en:"I hope you can give me a hand",kk:"/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /ə/ /hænd/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"它是非常重要的对我来说",en:"it is very important for me",kk:"/ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:"因为它是非常重要的对我来说",en:"because it is very important for me",kk:"/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/"},{zh:`我希望你可以给我搭把手因为\r
+它是非常重要的对我来说`,en:`I hope you can give me a hand \r
+because it is very important for me`,kk:`/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /ə/ /hænd/ \r
+/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/`},{zh:"而且",en:"and",kk:"/ənd/"},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"帮助",en:"to help",kk:"/tə/ /hɛlp/"},{zh:"我需要你帮助我",en:"I need you to help me",kk:"/aɪ/ /nid/ /ju/ /tə/ /hɛlp/ /mi/"},{zh:"现在",en:"now",kk:"/naʊ/"},{zh:"我需要你现在帮助我",en:"I need you to help me now",kk:"/aɪ/ /nid/ /ju/ /tə/ /hɛlp/ /mi/ /naʊ/"},{zh:`我希望你可以给我搭把手因为\r
+它是非常重要的对我来说而且\r
+我需要你现在帮助我`,en:`I hope you can give me a hand \r
+because it is very important for me \r
+and I need you to help me now`,kk:`/aɪ/ /hop/ /ju/ /kæn/ /ɡɪv/ /mi/ /ə/ /hænd/ \r
+/bɪ'kɔz/ /ɪt/ /ɪz/ /ˈvɛri/ /ɪm'pɔrtnt/ /fɝ/ /mi/ \r
+/ənd/ /aɪ/ /nid/ /ju/ /tə/ /hɛlp/ /mi/ /naʊ/`},{zh:"建议",en:"advice",kk:"/əd'vaɪs/"},{zh:"一些",en:"some",kk:"/səm/"},{zh:"一些建议",en:"some advice",kk:"/səm/ /əd'vaɪs/"},{zh:"你可以给我",en:"you can give me",kk:"/ju/ /kæn/ /ɡɪv/ /mi/"},{zh:"你可以给我一些建议",en:"you can give me some advice",kk:"/ju/ /kæn/ /ɡɪv/ /mi/ /səm/ /əd'vaɪs/"},{zh:"我希望",en:"I hope",kk:"/aɪ/ /hop/"},{zh:"我希望你可以给我一些建议",en:`I  hope  you  can  give  me  some \r
+advice`,kk:`/aɪ/ /hop/  /ju/  /kæn/  /ɡɪv/  /mi/  /səm/ \r
+/əd'vaɪs/`},{zh:"我的",en:"my",kk:"/maɪ/"},{zh:"我的工作",en:"my work",kk:"/maɪ/ /wɝk/"},{zh:"关于",en:"about",kk:"/ə'baʊt/"},{zh:"关于我的工作",en:"about my work",kk:"/ə'baʊt/ /maɪ/ /wɝk/"},{zh:`我希望你可以给我关于我的工\r
+作的一些建议`,en:`I  hope  you  can  give  me  some \r
+advice about my work`,kk:`/aɪ/ /hop/  /ju/  /kæn/  /ɡɪv/  /mi/  /səm/ \r
+/əd'vaɪs/ /ə'baʊt/ /maɪ/ /wɝk/`},{zh:"我可以给你一些建议",en:"I can give you some advice",kk:"/aɪ/ /kæn/ /ɡɪv/ /ju/ /səm/ /əd'vaɪs/"},{zh:"想要",en:"want to",kk:"/wɑnt/ /tə/"},{zh:"你的",en:"your",kk:"/jʊr/"},{zh:"关于",en:"about",kk:"/ə'baʊt/"},{zh:"关于你的工作",en:"about your work",kk:"/ə'baʊt/ /jʊr/ /wɝk/"},{zh:`我想要给你关于你的工作的一\r
+些建议`,en:`I want to give you some advice \r
+about your work`,kk:`/aɪ/ /wɑnt/ /tə/ /ɡɪv/ /ju/ /səm/ /əd'vaɪs/ \r
+/ə'baʊt/ /jʊr/ /wɝk/`},{zh:"我想要你给我一些建议",en:"I want you to give me some advice",kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /ɡɪv/ /mi/ /səm/ /əd'vaɪs/"},{zh:"关于我的工作",en:"about my work",kk:"/ə'baʊt/ /maɪ/ /wɝk/"},{zh:`我想要你给我关于我的工作的\r
+一些建议`,en:`I want you to give me some advice \r
+about my work`,kk:`/aɪ/ /wɑnt/ /ju/ /tə/ /ɡɪv/ /mi/ /səm/ /əd'vaɪs/ \r
+/ə'baʊt/ /maɪ/ /wɝk/`},{zh:"他的",en:"his",kk:"/hɪz/"},{zh:"他的工作",en:"his work",kk:"/hɪz/ /wɝk/"},{zh:"给他",en:"to give him",kk:"/tə/ /ɡɪv/ /hɪm/"},{zh:`你可以给他关于他的工作的一\r
+些建议`,en:`you  can  give  him  some advice \r
+about his work`,kk:`/ju/ /kæn/ /ɡɪv/ /mi/ /səm/ /əd'vaɪs/ /ə'baʊt/ \r
+/hɪz/ /wɝk/`},{zh:`我希望你可以给他关于他的工\r
+作的一些建议`,en:`I hope you can give him some \r
+advice about his work`,kk:`/aɪ/ /hop/  /ju/  /kæn/  /ɡɪv/  /mi/  /səm/ \r
+/əd'vaɪs/ /ə'baʊt/ /hɪz/ /wɝk/`},{zh:"她的",en:"her",kk:"/hɚ/"},{zh:"她的工作",en:"her work",kk:"/hɚ/ /wɝk/"},{zh:"给她",en:"to give her",kk:"/tə/ /ɡɪv/ /hɚ/"},{zh:`我希望你可以给她关于她的工\r
+作的一些建议`,en:`I  hope  you  can  give  her  some \r
+advice about her work`,kk:`/aɪ/ /hop/  /ju/  /kæn/  /ɡɪv/  /hɚ/ /səm/ \r
+/əd'vaɪs/ /ə'baʊt/ /hɚ/ /wɝk/`},{zh:`我希望她可以给我关于我的工\r
+作的一些建议`,en:`I  hope  she  can  give  me  some \r
+advice about my work`,kk:`/aɪ/ /hop/ /ʃi/ /kæn/ /ɡɪv/ /mi/ /səm/ /əd'vaɪs/ \r
+/ə'baʊt/ /maɪ/ /wɝk/`},{zh:"使用",en:"to use",kk:"/tə/ /juz/"},{zh:"字典",en:"the dictionary",kk:"/ðə/ /'dɪkʃənɛri/"},{zh:"使用字典",en:"to use the dictionary",kk:"/tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"我需要使用字典",en:"I need to use the dictionary",kk:"/aɪ/ /nid/ /tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"我不需要使用字典",en:"I don't need to use the dictionary",kk:"/aɪ/ /dont/ /nid/ /tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"我必须使用字典",en:"I have to use the dictionary",kk:"/aɪ/ /hæv/ /tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"我不必使用字典",en:"I don't have to use the dictionary",kk:"/aɪ/ /dont/ /hæv/ /tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"我的",en:"my",kk:"/maɪ/"},{zh:"我的字典",en:"my dictionary",kk:"/maɪ/ /'dɪkʃənɛri/"},{zh:"我想要",en:"I want",kk:"/aɪ/ /wɑnt/"},{zh:"我想要使用我的字典",en:"I want to use my dictionary",kk:"/aɪ/ /wɑnt/ /tə/ /juz/ /maɪ/ /'dɪkʃənɛri/"},{zh:"我不想使用我的字典",en:"I don't want to use my dictionary",kk:"/aɪ/ /dont/ /wɑnt/ /tə/ /juz/ /maɪ/ /'dɪkʃənɛri/"},{zh:"我想要你使用我的字典",en:"I want you to use my dictionary",kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /juz/ /maɪ/ /'dɪkʃənɛri/"},{zh:"我不想要你使用我的字典",en:`I  don't  want  you  to  use  my \r
+dictionary`,kk:`/aɪ/ /dont/  /wɑnt/  /ju/  /tə/ /juz/  /maɪ/ \r
+/'dɪkʃənɛri/`},{zh:"他的字典",en:"his dictionary",kk:"/hɪz/ /'dɪkʃənɛri/"},{zh:"我想要你使用他的字典",en:"I want you to use his dictionary",kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /juz/ /hɪz/ /'dɪkʃənɛri/"},{zh:"我不想要你使用他的字典",en:`I  don't  want  you  to  use  his \r
+dictionary`,kk:`/aɪ/ /dont/  /wɑnt/  /ju/  /tə/ /juz/  /hɪz/ \r
+/'dɪkʃənɛri/`},{zh:"她的",en:"her",kk:"/hɚ/"},{zh:"她的字典",en:"her dictionary",kk:"/hɚ/ /'dɪkʃənɛri/"},{zh:"我想要你使用她的字典",en:"I want you to use her dictionary",kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"我不想要你使用她的字典",en:`I  don't  want  you  to  use  her \r
+dictionary`,kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"需要",en:"need",kk:"/nid/"},{zh:"你的字典",en:"your dictionary",kk:"/jʊr/ /'dɪkʃənɛri/"},{zh:"我需要使用你的字典",en:"I need to use your dictionary",kk:"/aɪ/ /nid/ /tə/ /juz/ /jʊr/ /'dɪkʃənɛri/"},{zh:"我不需要使用你的字典",en:"I don't need to use your dictionary",kk:"/aɪ/ /dont/ /nid/ /tə/ /juz/ /jʊr/ /'dɪkʃənɛri/"},{zh:"使用",en:"to use",kk:"/tə/ /juz/"},{zh:"她必须使用",en:"she has to use",kk:"/ʃi/ /hæz/ /tə/ /juz/"},{zh:"她的字典",en:"her dictionary",kk:"/hɚ/ /'dɪkʃənɛri/"},{zh:"她必须使用她的字典",en:"she has to use her dictionary",kk:"/ʃi/ /hæz/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"她不必使用她的字典",en:`she  doesn't  have  to  use  her \r
+dictionary`,kk:"/ʃi/ /ˈdʌznt/ /hæv/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"她想要使用她的字典",en:"she wants to use her dictionary",kk:"/ʃi/ /wɑnts/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"她不想使用她的字典",en:`she  doesn't  want  to use  her \r
+dictionary`,kk:"/ʃi/ /ˈdʌznt/ /wɑnt/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"她想要我使用她的字典",en:"she wants me to use her dictionary",kk:"/ʃi/ /wɑnts/ /mi/ /tə/ /juz/ /hɚ/ /'dɪkʃənɛri/"},{zh:"她不想要我使用她的字典",en:`she doesn't want me to use her \r
+dictionary`,kk:`/ʃi/  /ˈdʌznt/  /wɑnt/  /mi/  /tə/ /juz/  /hɚ/ \r
+/'dɪkʃənɛri/`},{zh:"如何",en:"how",kk:"/haʊ/"},{zh:"如何使用字典",en:"how to use the dictionary",kk:"/haʊ/ /tə/ /juz/ /ðə/ /'dɪkʃənɛri/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"我需要知道如何使用字典",en:`I need to know how to use the \r
+dictionary`,kk:`/aɪ/ /nid/  /tə/ /no/  /haʊ/ /tə/ /juz/  /ðə/ \r
+/'dɪkʃənɛri/`},{zh:"我必须知道如何使用字典",en:`I have to know how to use the \r
+dictionary`,kk:`/aɪ/ /hæv/ /tə/ /no/ /haʊ/ /tə/ /juz/ /ðə/ \r
+/'dɪkʃənɛri/`},{zh:"我想要知道如何使用字典",en:`I want to know how to use the \r
+dictionary`,kk:`/aɪ/ /wɑnt/ /tə/ /no/ /haʊ/ /tə/ /juz/ /ðə/ \r
+/'dɪkʃənɛri/`},{zh:"电话",en:"the phone",kk:"/ðə/ /fon/"},{zh:"使用电话",en:"to use the phone",kk:"/tə/ /juz/ /ðə/ /fon/"},{zh:"我需要使用电话",en:"I need to use the phone",kk:"/aɪ/ /nid/ /tə/ /juz/ /ðə/ /fon/"},{zh:"如何使用电话",en:"how to use the phone",kk:"/haʊ/ /tə/ /juz/ /ðə/ /fon/"},{zh:"我需要知道如何使用电话",en:`I need to know how to use the \r
+phone`,kk:"/aɪ/ /nid/ /tə/ /no/ /haʊ/ /tə/ /juz/ /ðə/ /fon/"},{zh:"你的",en:"your",kk:"/jʊr/"},{zh:"你的电话",en:"your phone",kk:"/jʊr/ /fon/"},{zh:"我需要使用你的电话",en:"I need to use your phone",kk:"/aɪ/ /nid/ /tə/ /juz/ /jʊr/ /fon/"},{zh:"我不需要使用你的电话",en:"I don't need to use your phone",kk:"/aɪ/ /dont/ /nid/ /tə/ /juz/ /jʊr/ /fon/"},{zh:"他需要",en:"he needs",kk:"/hi/ /nidz/"},{zh:"她的",en:"her",kk:"/hɚ/"},{zh:"她的电话",en:"her phone",kk:"/hɚ/ /fon/"},{zh:"他需要使用她的电话",en:"he needs to use her phone",kk:"/hi/ /nidz/ /tə/ /juz/ /hɚ/ /fon/"},{zh:"他不需要使用她的电话",en:"he doesn't need to use her phone",kk:"/hi/ /ˈdʌznt/ /nid/ /tə/ /juz/ /hɚ/ /fon/"},{zh:"洗手间",en:"the bathroom",kk:"/ðə/ /'bæθrum/"},{zh:"使用洗手间",en:"to use the bathroom",kk:"/tə/ /juz/ /ðə/ /'bæθrum/"},{zh:"我需要",en:"I need",kk:"/aɪ/ /nid/"},{zh:"我需要使用洗手间",en:"I need to use the bathroom",kk:"/aɪ/ /nid/ /tə/ /juz/ /ðə/ /'bæθrum/"},{zh:"只是",en:"just",kk:"/dʒʌst/"},{zh:"我只是需要使用洗手间",en:"I just need to use the bathroom",kk:"/aɪ/ /dʒʌst/ /nid/ /tə/ /juz/ /ðə/ /'bæθrum/"},{zh:"你的",en:"your",kk:"/jʊr/"},{zh:"你的洗手间",en:"your bathroom",kk:"/jʊr/ /'bæθrum/"},{zh:"我只是需要使用你的洗手间",en:"I just need to use your bathroom",kk:"/aɪ/ /dʒʌst/ /nid/ /tə/ /juz/ /jʊr/ /'bæθrum/"},{zh:"他的洗手间",en:"his bathroom",kk:"/hɪz/ /'bæθrum/"},{zh:"我只是需要使用他的洗手间",en:"I just need to use his bathroom",kk:"/aɪ/ /dʒʌst/ /nid/ /tə/ /juz/ /hɪz/ /'bæθrum/"},{zh:"她的洗手间",en:"her bathroom",kk:"/hɚ/ /'bæθrum/"},{zh:"我只是需要使用她的洗手间",en:"I just need to use her bathroom",kk:"/aɪ/ /dʒʌst/ /nid/ /tə/ /juz/ /hɚ/ /'bæθrum/"},{zh:"他们的",en:"their",kk:"/ðɛr/"},{zh:"他们的洗手间",en:"their bathroom",kk:"/ðɛr/ /'bæθrum/"},{zh:"我只是需要使用他们的洗手间",en:"I just need to use their bathroom",kk:"/aɪ/ /dʒʌst/ /nid/ /tə/ /juz/ /ðɛr/ /'bæθrum/"},{zh:"我想要使用他们的洗手间",en:"I want to use their bathroom",kk:"/aɪ/ /wɑnt/ /tə/ /juz/ /ðɛr/ /'bæθrum/"},{zh:"我必须使用他们的洗手间",en:"I have to use their bathroom",kk:"/aɪ/ /hæv/ /tə/ /juz/ /ðɛr/ /'bæθrum/"}];return(d,w)=>{const t=o("StatementGroup");return i(),k("div",null,[a,u,s,h(t,{data:e})])}}});export{y as __pageData,j as default};
