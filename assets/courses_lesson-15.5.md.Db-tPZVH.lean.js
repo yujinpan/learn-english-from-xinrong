8c6c5c0dc382f20def61f95d034930a0c73542a0
yujinpan <yujinpan@users.noreply.github.com>
2024-10-21 10:12:01 +0000

    deploy: 9a1f50871af13012a70d851156f2564cfc52bcc4

---
+++ assets/courses_lesson-15.5.md.Db-tPZVH.lean.js
@@ -0,0 +1,56 @@
+import{D as n,c as h,I as k,a6 as t,o as a}from"./chunks/framework.CIJOd5-K.js";const s=t("",9),o=t("",1),g=JSON.parse('{"title":"Lesson-15.5","description":"","frontmatter":{},"headers":[],"relativePath":"courses/lesson-15.5.md","filePath":"courses/lesson-15.5.md","lastUpdated":null}'),r={name:"courses/lesson-15.5.md"},m=Object.assign(r,{setup(i){const e=[{zh:"吃",en:"to eat",kk:"/tə/ /it/"},{zh:"鸡蛋",en:"eggs",kk:"/ɛgz/"},{zh:"吃鸡蛋",en:"to eat eggs",kk:"/tə/ /it/ /ɛgz/"},{zh:"正在吃",en:"eating",kk:"/'itɪŋ/"},{zh:"正在吃鸡蛋",en:"eating eggs",kk:"/'itɪŋ/ /ɛgz/"},{zh:"我是",en:"I am",kk:"/aɪ/ /æm/"},{zh:"我是星荣",en:"I am Xingrong",kk:"/aɪ/ /æm/"},{zh:"正在吃鸡蛋",en:"eating eggs",kk:"/'itɪŋ/ /ɛgz/"},{zh:"我正在吃鸡蛋",en:"I am eating eggs",kk:"/aɪ/ /æm/ /'itɪŋ/ /ɛgz/"},{zh:"厨房",en:"the kitchen",kk:"/ðə/ /'kɪtʃɪn/"},{zh:"在厨房里",en:"in the kitchen",kk:"/ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:`我在厨房里（现在）正在吃鸡
+蛋`,en:"I am eating eggs in the kitchen",kk:"/aɪ/ /æm/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"（I，现在时）be",en:"am",kk:"/æm/"},{zh:"（I，过去时）be",en:"was",kk:"/wəz/"},{zh:`我在厨房里（过去）正在吃鸡
+蛋`,en:"I was eating eggs in the kitchen",kk:"/aɪ/ /wəz/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"（you，现在时）be",en:"are",kk:"/ɚ/"},{zh:`你在厨房里（现在）正在吃鸡
+蛋`,en:"you are eating eggs in the kitchen",kk:"/ju/ /ɚ/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"（you，过去时）be",en:"were",kk:"/wɚ/"},{zh:`你在厨房里（过去）正在吃鸡
+蛋`,en:"you were eating eggs in the kitchen",kk:"/ju/ /wɚ/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"他们",en:"they",kk:"/ðe/"},{zh:"（they，现在时）be",en:"are",kk:"/ɚ/"},{zh:`他们在厨房里（现在）正在吃
+鸡蛋`,en:"they are eating eggs in the kitchen",kk:"/ðe/ /ɚ/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"（they，过去时）be",en:"were",kk:"/wɚ/"},{zh:`他们在厨房里（过去）正在吃
+鸡蛋`,en:`they were eating eggs in the
+kitchen`,kk:"/ðe/ /wɚ/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"喝",en:"to drink",kk:"/tə/ /drɪŋk/"},{zh:"茶",en:"tea",kk:"/ti/"},{zh:"喝茶",en:"to drink tea",kk:"/tə/ /drɪŋk/ /ti/"},{zh:"正在喝",en:"drinking",kk:"/'drɪŋkɪŋ/"},{zh:"正在喝茶",en:"drinking tea",kk:"/'drɪŋkɪŋ/ /ti/"},{zh:"他们",en:"they",kk:"/ðe/"},{zh:"（they，现在时）be",en:"are",kk:"/ɚ/"},{zh:"他们（现在）正在喝茶",en:"they are drinking tea",kk:"/ðe/ /ɚ/ /'drɪŋkɪŋ/ /ti/"},{zh:"花园",en:"the garden",kk:"/ðə/ /ˈɡɑrdn/"},{zh:"在花园里",en:"in the garden",kk:"/ɪn/ /ðə/ /ˈɡɑrdn/"},{zh:"他们在花园里（现在）正在喝",en:"they are drinking tea in the garden",kk:"/ðe/ /ɚ/ /'drɪŋkɪŋ/ /ti/ /ɪn/ /ðə/ /ˈɡɑrdn/"},{zh:`茶
+（they，过去时）be`,en:"were",kk:"/wɚ/"},{zh:`他们在花园里（过去）正在喝
+茶`,en:`they were drinking tea in the
+garden`,kk:"/ðe/ /wɚ/ /'drɪŋkɪŋ/ /ti/ /ɪn/ /ðə/ /ˈɡɑrdn/"},{zh:`我在厨房里（过去）正在吃鸡
+蛋`,en:"I was eating eggs in the kitchen",kk:"/aɪ/ /wəz/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/ /'kɪtʃɪn/"},{zh:"与此同时",en:"while",kk:"/waɪl/"},{zh:`他们在花园里（过去）正在喝
+茶与此同时我在厨房里（过
+去）正在吃鸡蛋`,en:`they were drinking tea in the
+garden while I was eating eggs in
+the kitchen`,kk:`/ðe/ /wɚ/ /'drɪŋkɪŋ/ /ti/ /ɪn/ /ðə/ /ˈɡɑrdn/
+/waɪl/ /aɪ/ /wəz/ /'itɪŋ/ /ɛgz/ /ɪn/ /ðə/
+/'kɪtʃɪn/`},{zh:"教",en:"to teach",kk:"/tə/ /titʃ/"},{zh:"我们",en:"us",kk:"/ʌs/"},{zh:"教我们",en:"to teach us",kk:"/tə/ /titʃ/ /ʌs/"},{zh:"正在教",en:"teaching",kk:"/'titʃɪŋ/"},{zh:"正在教我们",en:"teaching us",kk:"/'titʃɪŋ/ /ʌs/"},{zh:"说",en:"to speak",kk:"/tə/ /spik/"},{zh:"英语",en:"English",kk:"/ˈɪŋɡlɪʃ/"},{zh:"说英语",en:"to speak English",kk:"/tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"如何",en:"how",kk:"/haʊ/"},{zh:"如何说英语",en:"how to speak English",kk:"/haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"正在教我们如何说英语",en:"teaching us how to speak English",kk:"/'titʃɪŋ/ /ʌs/ /haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"（he，现在时）be",en:"is",kk:"/ɪz/"},{zh:"星荣正在教我们如何说英语",en:`Xingrong is teaching us how to
+speak English`,kk:"/ɪz/ /'titʃɪŋ/ /ʌs/ /haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"（he，过去时）be",en:"was",kk:"/wəz/"},{zh:`星荣（过去）正在教我们如何
+说英语`,en:`Xingrong was teaching us how to
+speak English`,kk:"/wəz/ /'titʃɪŋ/ /ʌs/ /haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"学习",en:"to learn",kk:"/tə/ /lɝn/"},{zh:"英语",en:"English",kk:"/ˈɪŋɡlɪʃ/"},{zh:"学习英语",en:"to learn English",kk:"/tə/ /lɝn/ /ˈɪŋɡlɪʃ/"},{zh:"正在学习",en:"learning",kk:"/'lɝnɪŋ/"},{zh:"正在学习英语",en:"learning English",kk:"/'lɝnɪŋ/ /ˈɪŋɡlɪʃ/"},{zh:"（I，现在时）be",en:"am",kk:"/æm/"},{zh:"我正在学习英语",en:"I am learning English",kk:"/aɪ/ /æm/ /'lɝnɪŋ/ /ˈɪŋɡlɪʃ/"},{zh:"视频",en:"videos",kk:"/'vɪdɪoz/"},{zh:"他的",en:"his",kk:"/hɪz/"},{zh:"他的视频",en:"his videos",kk:"/hɪz/ /'vɪdɪoz/"},{zh:"观看",en:"to watch",kk:"/tə/ /wɑtʃ/"},{zh:"观看他的视频",en:"to watch his videos",kk:"/tə/ /wɑtʃ/ /hɪz/ /'vɪdɪoz/"},{zh:"正在观看",en:"watching",kk:"/'wɑtʃɪŋ/"},{zh:"正在观看他的视频",en:"watching his videos",kk:"/'wɑtʃɪŋ/ /hɪz/ /'vɪdɪoz/"},{zh:"通过……的方式",en:"by",kk:"/baɪ/"},{zh:"通过观看他的视频的方式",en:"by watching his videos",kk:"/baɪ/ /'wɑtʃɪŋ/ /hɪz/ /'vɪdɪoz/"},{zh:"我正在学习英语",en:"I am learning English",kk:"/aɪ/ /æm/ /'lɝnɪŋ/ /ˈɪŋɡlɪʃ/"},{zh:`我正在通过观看他的视频的方
+式学习英语`,en:`I am learning English by watching
+his videos`,kk:`/aɪ/ /æm/ /'lɝnɪŋ/ /ˈɪŋɡlɪʃ/ /baɪ/ /'wɑtʃɪŋ/
+/hɪz/ /'vɪdɪoz/`},{zh:"（I，过去时）be",en:"was",kk:"/wəz/"},{zh:`我（过去）正在通过观看他的
+视频的方式学习英语`,en:`I was learning English by watching
+his videos`,kk:`/aɪ/ /wəz/ /'lɝnɪŋ/ /ˈɪŋɡlɪʃ/ /baɪ/ /'wɑtʃɪŋ/
+/hɪz/ /'vɪdɪoz/`},{zh:`星荣（过去）正在教我们如何
+说英语`,en:`Xingrong was teaching us how to
+speak English`,kk:"/wəz/ /'titʃɪŋ/ /ʌs/ /haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/"},{zh:"与此同时",en:"while",kk:"/waɪl/"},{zh:`星荣（过去）正在教我们如何
+说英语与此同时我（过去）正
+在通过观看他的视频的方式学
+习英语`,en:`Xingrong was teaching us how to
+speak English while I was learning
+English by watching his videos`,kk:`/wəz/ /'titʃɪŋ/ /ʌs/ /haʊ/ /tə/ /spik/ /ˈɪŋɡlɪʃ/
+/waɪl/ /aɪ/ /wəz/ /'lɝnɪŋ/ /ˈɪŋɡlɪʃ/ /baɪ/
+/'wɑtʃɪŋ/ /hɪz/ /'vɪdɪoz/`},{zh:"走路",en:"to walk",kk:"/tə/ /wɔk/"},{zh:"家",en:"home",kk:"/hom/"},{zh:"走路回家",en:"to walk home",kk:"/tə/ /wɔk/ /hom/"},{zh:"学校",en:"school",kk:"/skul/"},{zh:"从",en:"from",kk:"/frʌm/"},{zh:"从学校",en:"from school",kk:"/frʌm/ /skul/"},{zh:"从学校走路回家",en:"to walk home from school",kk:"/tə/ /wɔk/ /hom/ /frʌm/ /skul/"},{zh:"正在从学校走路回家",en:"walking home from school",kk:"/'wɔkɪŋ/ /hom/ /frʌm/ /skul/"},{zh:"（I，现在时）be",en:"am",kk:"/æm/"},{zh:"我正在从学校走路回家",en:"I am walking home from school",kk:"/aɪ/ /æm/ /'wɔkɪŋ/ /hom/ /frʌm/ /skul/"},{zh:"（I，过去时）be",en:"was",kk:"/wəz/"},{zh:`我（过去）正在从学校走路回
+家`,en:"I was walking home from school",kk:"/aɪ/ /wəz/ /'wɔkɪŋ/ /hom/ /frʌm/ /skul/"},{zh:"朋友",en:"friend",kk:"/frɛnd/"},{zh:"老的",en:"old",kk:"/old/"},{zh:"一个老朋友",en:"an old friend",kk:"/ən/ /old/ /frɛnd/"},{zh:"遇见",en:"to meet",kk:"/tə/ /mit/"},{zh:"遇见一个老朋友",en:"to meet an old friend",kk:"/tə/ /mit/ /ən/ /old/ /frɛnd/"},{zh:"（过去）遇见",en:"met",kk:"/mɛt/"},{zh:"（过去）遇见一个老朋友",en:"met an old friend",kk:"/mɛt/ /ən/ /old/ /frɛnd/"},{zh:"我（过去）遇见一个老朋友",en:"I met an old friend",kk:"/aɪ/ /mɛt/ /ən/ /old/ /frɛnd/"},{zh:`我（过去）正在从学校走路回
+家`,en:"I was walking home from school",kk:"/aɪ/ /wəz/ /'wɔkɪŋ/ /hom/ /frʌm/ /skul/"},{zh:"当……的时候",en:"when",kk:"/wɛn/"},{zh:`当我（过去）正在从学校走路
+回家的时候`,en:`when I was walking home from
+school`,kk:"/wɛn/ /aɪ/ /wəz/ /'wɔkɪŋ/ /hom/ /frʌm/ /skul/"},{zh:`当我（过去）正在从学校走路
+回家的时候我（过去）遇见一
+个老朋友`,en:`I met an old friend when I was
+walking home from school`,kk:`/aɪ/ /mɛt/ /ən/ /old/ /frɛnd/ /wɛn/ /aɪ/ /wəz/
+/'wɔkɪŋ/ /hom/ /frʌm/ /skul/`},{zh:"睡觉",en:"to sleep",kk:"/tə/ /slip/"},{zh:"卧室",en:"the bedroom",kk:"/ðə/ /'bɛdrum/"},{zh:"在卧室里",en:"in the bedroom",kk:"/ɪn/ /ðə/ /'bɛdrum/"},{zh:"在卧室里睡觉",en:"to sleep in the bed room",kk:"/tə/ /slip/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:"正在睡觉",en:"sleeping",kk:"/ˈslipɪŋ/"},{zh:"正在在卧室里睡觉",en:"sleeping in the bedroom",kk:"/ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:"（I，现在时）be",en:"am",kk:"/æm/"},{zh:"我正在在卧室里睡觉",en:"I am sleeping in the bedroom",kk:"/aɪ/ /æm/ /ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:"（I，过去时）be",en:"was",kk:"/wəz/"},{zh:"我（过去）正在在卧室里睡觉",en:"I was sleeping in the bedroom",kk:"/aɪ/ /wəz/ /ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:"打电话",en:"to call",kk:"/tə/ /kɔl/"},{zh:"打电话给我",en:"to call me",kk:"/tə/ /kɔl/ /mi/"},{zh:"（过去）打电话",en:"called",kk:"/kɔld/"},{zh:"（过去）打电话给我",en:"called me",kk:"/kɔld/ /mi/"},{zh:"她",en:"she",kk:"/ʃi/"},{zh:"她（过去）打电话给我",en:"she called me",kk:"/ʃi/ /kɔld/ /mi/"},{zh:"当……的时候",en:"when",kk:"/wɛn/"},{zh:`当她（过去）打电话给我的时
+候`,en:"when she called me",kk:"/wɛn/ /ʃi/ /kɔld/ /mi/"},{zh:"我（过去）正在在卧室里睡觉",en:"I was sleeping in the bedroom",kk:"/aɪ/ /wəz/ /ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:`当她打电话给我的时候我（过
+去）正在在卧室里睡觉`,en:`I was sleeping in the bedroom
+when she called me`,kk:`/aɪ/ /wəz/ /ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/ /wɛn/
+/ʃi/ /kɔld/ /mi/`},{zh:"玩",en:"to play",kk:"/tə/ /ple/"},{zh:"钢琴",en:"the piano",kk:"/ðə/ /pɪ'æno/"},{zh:"弹钢琴",en:"to play the piano",kk:"/tə/ /ple/ /ðə/ /pɪ'æno/"},{zh:"正在玩",en:"playing",kk:"/pleɪŋ/"},{zh:"正在弹钢琴",en:"playing the piano",kk:"/pleɪŋ/ /ðə/ /pɪ'æno/"},{zh:"（她，现在时）be",en:"is",kk:"/ɪz/"},{zh:"她正在弹钢琴",en:"she is playing the piano",kk:"/ʃi/ /ɪz/ /pleɪŋ/ /ðə/ /pɪ'æno/"},{zh:"（她，过去时）be",en:"was",kk:"/wəz/"},{zh:"她（过去）正在弹钢琴",en:"she was playing the piano",kk:"/ʃi/ /wəz/ /pleɪŋ/ /ðə/ /pɪ'æno/"},{zh:"起居室；客厅",en:"the living room",kk:"/ðə/ /'lɪvɪŋ/ /rum/"},{zh:"在客厅",en:"in the living room",kk:"/ɪn/ /ðə/ /'lɪvɪŋ/ /rum/"},{zh:"她（过去）正在在客厅弹钢琴",en:`she was playing the piano in the
+living room`,kk:`/ʃi/ /wəz/ /pleɪŋ/ /ðə/ /pɪ'æno/ /ɪn/ /ðə/
+/'lɪvɪŋ/ /rum/`},{zh:"与此同时",en:"while",kk:"/waɪl/"},{zh:"我（过去）正在在卧室里睡觉",en:"I was sleeping in the bedroom",kk:"/aɪ/ /wəz/ /ˈslipɪŋ/ /ɪn/ /ðə/ /'bɛdrum/"},{zh:`她（过去）正在在客厅弹钢琴
+与此同时我（过去）正在在卧
+室里睡觉`,en:`she was playing the piano in the
+living room while I was sleeping in
+the bedroom`,kk:`/ʃi/ /wəz/ /pleɪŋ/ /ðə/ /pɪ'æno/ /ɪn/ /ðə/
+/'lɪvɪŋ/ /rum/ /waɪl/ /aɪ/ /wəz/ /ˈslipɪŋ/ /ɪn/
+/ðə/ /'bɛdrum/`}];return(l,z)=>{const d=n("StatementGroup");return a(),h("div",null,[s,k(d,{data:e}),o])}}});export{g as __pageData,m as default};
---
+++ assets/courses_lesson-15.md.DTq0voVT.js
@@ -0,0 +1,84 @@
+import{D as n,c as k,I as s,j as t,a as i,a6 as h,o as a}from"./chunks/framework.CIJOd5-K.js";const r=t("h1",{id:"lesson-15",tabindex:"-1"},[i("Lesson-15 "),t("a",{class:"header-anchor",href:"#lesson-15","aria-label":'Permalink to "Lesson-15"'},"​")],-1),o=t("p",null,"这里解释一个视频里说过的小问题。I want you to finish your homework. 这句话 的 finish 前面为什么要加 to 的问题。我之前解释说人称后面的动词不需要加 to，可 能说的不严谨，让很多人误以为因为 finish 前面有 you 这个人称词，所以也不用加 to，这其实是错误的。我说人称词后面的动词不用加 to，真正想表达的意思是位于 句首的主格人称和他后面的第一个动作词之间不需要 to，就像 I 和 want 中间不需 要 to。而 finish 前面的 to 是需要的，因为在这里 finish 前面有一个动词 want，finish 已经属于第二个动词了，不管它前面有没有人称，这个 to 都是需要加上的。所以 动词前要不要加 to 不是单独看前面是不是人称词，还要看这个人称词是不是第一 个出现的主格人称。 星荣 微信：xingrong-english 公众号：Hi 要大声说出来",-1),l=h('<table tabindex="0"><thead><tr><th>中文</th><th>原形</th><th>第三人称单数</th><th>过去式</th></tr></thead><tbody><tr><td>想要</td><td>want</td><td>wants</td><td>wanted</td></tr><tr><td>喜欢</td><td>like</td><td>likes</td><td>liked</td></tr><tr><td>有；(+to)不得不</td><td>have</td><td>has</td><td>had</td></tr><tr><td>计划</td><td>plan</td><td>plans</td><td>planned</td></tr><tr><td>需要</td><td>need</td><td>needs</td><td>needed</td></tr><tr><td>做</td><td>do</td><td>does</td><td>did</td></tr><tr><td>告诉</td><td>tell</td><td>tells</td><td>told</td></tr><tr><td>说话</td><td>talk</td><td>talks</td><td>talked</td></tr><tr><td>是</td><td>be（am）</td><td>is</td><td>was</td></tr><tr><td>是</td><td>be（is）</td><td>is</td><td>was</td></tr><tr><td>是</td><td>be（are）</td><td>is</td><td>were</td></tr><tr><td>看到</td><td>see</td><td>sees</td><td>saw</td></tr><tr><td>吃</td><td>eat</td><td>eats</td><td>ate</td></tr><tr><td>喝</td><td>drink</td><td>drinks</td><td>drank</td></tr><tr><td>知道</td><td>know</td><td>knows</td><td>knew</td></tr><tr><td>忘记</td><td>forget</td><td>forgets</td><td>forgot</td></tr><tr><td>失去</td><td>lose</td><td>loses</td><td>lost</td></tr><tr><td>离开</td><td>leave</td><td>leaves</td><td>left</td></tr><tr><td>睡觉</td><td>sleep</td><td>sleeps</td><td>slept</td></tr><tr><td>明白</td><td>understand</td><td>understands</td><td>understood</td></tr><tr><td>买</td><td>buy</td><td>buys</td><td>bought</td></tr><tr><td>洗</td><td>wash</td><td>washes</td><td>washed</td></tr><tr><td>打电话</td><td>call</td><td>calls</td><td>called</td></tr><tr><td>帮助</td><td>help</td><td>helps</td><td>helped</td></tr><tr><td>相信</td><td>believe</td><td>believes</td><td>believed</td></tr><tr><td>学习</td><td>study</td><td>studies</td><td>studied</td></tr><tr><td>解释</td><td>explain</td><td>explains</td><td>explained</td></tr><tr><td>问</td><td>ask</td><td>asks</td><td>asked</td></tr><tr><td>飞</td><td>fly</td><td>flies</td><td>flew</td></tr><tr><td>阅读</td><td>read</td><td>reads</td><td>read</td></tr><tr><td>走路</td><td>walk</td><td>walks</td><td>walked</td></tr><tr><td>跳舞</td><td>dance</td><td>dances</td><td>danced</td></tr><tr><td>回答</td><td>answer</td><td>answers</td><td>answered</td></tr><tr><td>工作</td><td>work</td><td>works</td><td>worked</td></tr><tr><td>待在</td><td>stay</td><td>stays</td><td>stayed</td></tr><tr><td>支付</td><td>pay</td><td>pays</td><td>paid</td></tr><tr><td>旅行</td><td>travel</td><td>travels</td><td>traveled</td></tr><tr><td>给</td><td>give</td><td>gives</td><td>gave</td></tr><tr><td>达到</td><td>get</td><td>gets</td><td>got</td></tr><tr><td>锁</td><td>lock</td><td>locks</td><td>locked</td></tr><tr><td>记得</td><td>remember</td><td>remembers</td><td>remembered</td></tr><tr><td>清理</td><td>clean</td><td>cleans</td><td>cleaned</td></tr><tr><td>找到</td><td>find</td><td>finds</td><td>found</td></tr><tr><td>关闭</td><td>close</td><td>closes</td><td>closed</td></tr><tr><td>教</td><td>teach</td><td>teaches</td><td>taught</td></tr><tr><td>展示</td><td>show</td><td>shows</td><td>showed</td></tr><tr><td>让</td><td>let</td><td>lets</td><td>let</td></tr><tr><td>休息</td><td>rest</td><td>rests</td><td>rested</td></tr><tr><td>用</td><td>use</td><td>uses</td><td>used</td></tr><tr><td>做饭</td><td>cook</td><td>cooks</td><td>cooked</td></tr><tr><td>去</td><td>go</td><td>goes</td><td>went</td></tr><tr><td>决定</td><td>decide</td><td>decides</td><td>decided</td></tr><tr><td>驾驶</td><td>drive</td><td>drives</td><td>drove</td></tr><tr><td>改变</td><td>change</td><td>changes</td><td>changed</td></tr><tr><td>尝试</td><td>try</td><td>tries</td><td>tried</td></tr><tr><td>停止</td><td>stop</td><td>stops</td><td>Stopped</td></tr><tr><td>参加</td><td>join</td><td>joins</td><td>joined</td></tr><tr><td>邀请</td><td>invite</td><td>invites</td><td>invited</td></tr><tr><td>拒绝</td><td>refuse</td><td>refuses</td><td>refused</td></tr><tr><td>接受</td><td>accept</td><td>accepts</td><td>accepted</td></tr><tr><td>学习</td><td>learn</td><td>learns</td><td>learnt</td></tr><tr><td>完成</td><td>finish</td><td>finishes</td><td>finished</td></tr><tr><td>看</td><td>watch</td><td>watches</td><td>watched</td></tr><tr><td>游泳</td><td>swim</td><td>swims</td><td>swam</td></tr><tr><td>掌握</td><td>master</td><td>masters</td><td>mastered</td></tr></tbody></table>',1),m=JSON.parse('{"title":"Lesson-15","description":"","frontmatter":{},"headers":[],"relativePath":"courses/lesson-15.md","filePath":"courses/lesson-15.md","lastUpdated":null}'),z={name:"courses/lesson-15.md"},p=Object.assign(z,{setup(f){const d=[{zh:"我们的",en:"our",kk:"/'aʊɚ/"},{zh:"团队",en:"team",kk:"/tim/"},{zh:"我们的团队",en:"our team",kk:"/'aʊɚ/ /tim/"},{zh:"加入",en:"to join",kk:"/tə/ /dʒɔɪn/"},{zh:"加入我们的团队",en:"to join our team",kk:"/tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:"决定",en:"to decide",kk:"/tə/ /dɪ'saɪd/"},{zh:"决定加入我们的团队",en:"to decide to join our team",kk:"/tə/ /dɪ'saɪd/ /tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:"他决定加入我们的团队",en:"he decides to join our team",kk:"/hi/ /dɪ'saɪdz/ /tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:`他（过去）决定加入我们的
+团队`,en:"he decided to join our team",kk:"/hi/ /dɪ'saɪdɪd/ /tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:"邀请",en:"to invite",kk:"/tə/ /ɪn'vaɪt/"},{zh:"邀请（名词）",en:"invitation",kk:"/ɪnvɪ'teʃən/"},{zh:"这个邀请",en:"the invitation",kk:"/ði/ /ɪnvɪ'teʃən/"},{zh:"接受",en:"to accept",kk:"/tə/ /ək'sɛpt/"},{zh:"接受这个邀请",en:"to accept the invitation",kk:"/tə/ /ək'sɛpt/ /ði/ /ɪnvɪ'teʃən/"},{zh:"加入我们的团队",en:"to join our team",kk:"/tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:"加入我们的团队的邀请",en:"the invitation to join our team",kk:"/ði/ /ɪnvɪ'teʃən/ /tə/ /dʒɔɪn/ /'aʊɚ/ /tim/"},{zh:"接受加入我们团队的邀请",en:`to accept the invitation to join our
+team`,kk:`/tə/ /ək'sɛpt/ /ði/ /ɪnvɪ'teʃən/ /tə/ /dʒɔɪn/
+/'aʊɚ/ /tim/`},{zh:"他接受加入我们团队的邀请",en:`he accepts the invitation to join
+our team`,kk:`/hi/ /ək'sɛpts/ /ði/ /ɪnvɪ'teʃən/ /tə/ /dʒɔɪn/
+/'aʊɚ/ /tim/`},{zh:`他不接受加入我们团队的邀
+请`,en:`he doesn't accept the invitation to
+join our team`,kk:`/hi/ /ˈdʌznt/ /ək'sɛpt/ /ði/ /ɪnvɪ'teʃən/ /tə/
+/dʒɔɪn/ /'aʊɚ/ /tim/`},{zh:`他接受了加入我们团队的邀
+请`,en:`he accepted the invitation to join
+our team`,kk:`/hi/ /ək'sɛptɪd/ /ði/ /ɪnvɪ'teʃən/ /tə/ /dʒɔɪn/
+/'aʊɚ/ /tim/`},{zh:`他（过去）没有接受加入我
+们团队的邀请`,en:`he didn't accept the invitation to
+join our team`,kk:`/hi/ /ˈdɪdnt/ /ək'sɛpt/ /ði/ /ɪnvɪ'teʃən/ /tə/
+/dʒɔɪn/ /'aʊɚ/ /tim/`},{zh:"这个礼物",en:"the gift",kk:"/ðə/ /ɡɪft/"},{zh:"接受",en:"to accept",kk:"/tə/ /ək'sɛpt/"},{zh:"接受这个礼物",en:"to accept the gift",kk:"/tə/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:"他",en:"he",kk:"/hi/"},{zh:"他接受这个礼物",en:"he accepts the gift",kk:"/hi/ /ək'sɛpts/ /ðə/ /ɡɪft/"},{zh:"他不接受这个礼物",en:"he doesn't accept the gift",kk:"/hi/ /ˈdʌznt/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:"他（过去）接受这个礼物",en:"he accepted the gift",kk:"/hi/ /ək'sɛptɪd/ /ðə/ /ɡɪft/"},{zh:`他（过去）没有接受这个礼
+物`,en:"he didn't accept the gift",kk:"/hi/ /ˈdɪdnt/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:"他想要",en:"he wants",kk:"/hi/ /wɑnts/"},{zh:"他想要接受这个礼物",en:"he wants to accept the gift",kk:"/hi/ /wɑnts/ /tə/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:"他不想接受这个礼物",en:"he doesn't want to accept the gift",kk:"/hi/ /ˈdʌznt/ /wɑnt/ /tə/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:`他（过去）想要接受这个礼
+物`,en:"he wanted to accept the gift",kk:"/hi/ /wɑntɪd/ /tə/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:`他（过去）不想接受这个礼
+物`,en:"he didn't want to accept the gift",kk:"/hi/ /ˈdɪdnt/ /wɑnt/ /tə/ /ək'sɛpt/ /ðə/ /ɡɪft/"},{zh:"完成",en:"to finish",kk:"/tə/ /'fɪnɪʃ/"},{zh:"你的",en:"your",kk:"/jʊr/"},{zh:"家庭作业",en:"homework",kk:"/'hom'wɝk/"},{zh:"你的家庭作业",en:"your homework",kk:"/jʊr/ /'hom'wɝk/"},{zh:"完成你的家庭作业",en:"to finish your homework",kk:"/tə/ /'fɪnɪʃ/ /jʊr/ /'hom'wɝk/"},{zh:"我想要你",en:"I want you",kk:"/aɪ/ /wɑnt/ /ju/"},{zh:"我想要你完成你的家庭作业",en:`I want you to finish your
+homework`,kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /'fɪnɪʃ/ /jʊr/ /'hom'wɝk/"},{zh:"看",en:"to watch",kk:"/tə/ /wɔtʃ/"},{zh:"电视",en:"TV",kk:"/ˈtiˈvi/"},{zh:"看电视",en:"to watch TV",kk:"/tə/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"你看电视",en:"you watch TV",kk:"/ju/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"在……之前",en:"before",kk:"/bɪ'fɔr/"},{zh:"在你看电视之前",en:"before you watch TV",kk:"/bɪ'fɔr/ /ju/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"我想要你完成你的家庭作业",en:`I want you to finish your
+homework`,kk:"/aɪ/ /wɑnt/ /ju/ /tə/ /'fɪnɪʃ/ /jʊr/ /'hom'wɝk/"},{zh:`我想要你完成你的家庭作业
+在你看电视之前`,en:`I want you to finish your
+homework before you watch TV`,kk:`/aɪ/ /wɑnt/ /ju/ /tə/ /'fɪnɪʃ/ /jʊr/ /'hom'wɝk/
+/bɪ'fɔr/ /ju/ /wɔtʃ/ /ˈtiˈvi/`},{zh:"可以",en:"can",kk:"/kæn/"},{zh:"你可以",en:"you can",kk:"/ju/ /kæn/"},{zh:"你可以看电视",en:"you can watch TV",kk:"/ju/ /kæn/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"你不可以看电视",en:"you can not watch TV",kk:"/ju/ /kæn/ /nɑt/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"你想要",en:"you want",kk:"/ju/ /wɑnt/"},{zh:"是否；如果",en:"if",kk:"/ɪf/"},{zh:"如果你想的话",en:"if you want",kk:"/ɪf/ /ju/ /wɑnt/"},{zh:"你可以看电视",en:"you can watch TV",kk:"/ju/ /kæn/ /wɔtʃ/ /ˈtiˈvi/"},{zh:"你可以看电视如果你想的话",en:"you can watch TV if you want",kk:"/ju/ /kæn/ /wɔtʃ/ /ˈtiˈvi/ /ɪf/ /ju/ /wɑnt/"},{zh:"回答",en:"to answer",kk:"/tə/ /'ænsɚ/"},{zh:"这个问题",en:"the question",kk:"/ðə/ /'kwɛstʃən/"},{zh:"回答这个问题",en:"to answer the question",kk:"/tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/"},{zh:"拒绝",en:"to refuse",kk:"/tə/ /ri'fjuz/"},{zh:"拒绝回答这个问题",en:"to refuse to answer the question",kk:"/tə/ /ri'fjuz/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/"},{zh:"我想拒绝回答这个问题",en:`I want to refuse to answer the
+question`,kk:`/aɪ/ /wɑnt/ /tə/ /ri'fjuz/ /tə/ /'ænsɚ/ /ðə/
+/'kwɛstʃən/`},{zh:"我拒绝回答这个问题",en:"I refuse to answer the question",kk:"/aɪ/ /ri'fjuz/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/"},{zh:`我（过去）拒绝回答这个问
+题`,en:"I refused to answer the question",kk:"/aɪ/ /ri'fjuzd/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/"},{zh:"这个问题",en:"the question",kk:"/ðə/ /'kwɛstʃən/"},{zh:"困难的",en:"difficult",kk:"/'dɪfɪkəlt/"},{zh:"这个问题是困难的",en:"the question is difficult",kk:"/ðə/ /'kwɛstʃən/ /ɪz/ /'dɪfɪkəlt/"},{zh:"这个问题（过去）是困难的",en:"the question was difficult",kk:"/ðə/ /'kwɛstʃən/ /wəz/ /'dɪfɪkəlt/"},{zh:"非常",en:"very",kk:"/ˈvɛri/"},{zh:`这个问题（过去）是非常困
+难的`,en:"the question was very difficult",kk:"/ðə/ /'kwɛstʃən/ /wəz/ /ˈvɛri/ /'dɪfɪkəlt/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:`这个问题（过去）是非常困
+难的对我来说`,en:`the question was very difficult for
+me`,kk:`/ðə/ /'kwɛstʃən/ /wəz/ /ˈvɛri/ /'dɪfɪkəlt/
+/fɝ/ /mi/`},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:`因为这个问题（过去）是非
+常困难的对我来说`,en:`because the question was very
+difficult for me`,kk:`/bɪ'kɔz/ /ðə/ /'kwɛstʃən/ /wəz/ /ˈvɛri/
+/'dɪfɪkəlt/ /fɝ/ /mi/`},{zh:`我（过去）拒绝回答这个问
+题`,en:"I refused to answer the question",kk:"/aɪ/ /ri'fjuzd/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/"},{zh:`我（过去）拒绝回答这个问
+题因为这个问题（过去）是
+非常困难的对我来说`,en:`I refused to answer the question
+because the question was very
+difficult for me`,kk:`/aɪ/ /ri'fjuzd/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/
+/bɪ'kɔz/ /ðə/ /'kwɛstʃən/ /wəz/ /ˈvɛri/
+/'dɪfɪkəlt/ /fɝ/ /mi/`},{zh:`我（过去）拒绝回答这个问
+题因为它（过去）是非常困
+难的对我来说`,en:`I refused to answer the question
+because it was very difficult for
+me`,kk:`/aɪ/ /ri'fjuzd/ /tə/ /'ænsɚ/ /ðə/ /'kwɛstʃən/
+/bɪ'kɔz/ /ɪt/ /wəz/ /ˈvɛri/ /'dɪfɪkəlt/ /fɝ/
+/mi/`},{zh:"困难的",en:"difficult",kk:"/'dɪfɪkəlt/"},{zh:"它是困难的",en:"it is difficult",kk:"/ɪt/ /ɪz/ /'dɪfɪkəlt/"},{zh:"完成",en:"to finish",kk:"/tə/ /'fɪnɪʃ/"},{zh:"这个工作",en:"the work",kk:"/ðə/ /wɝk/"},{zh:"完成这个工作",en:"to finish the work",kk:"/tə/ /'fɪnɪʃ/ /ðə/ /wɝk/"},{zh:`完成这个工作（它）是困难
+的`,en:"it is difficult to finish the work",kk:"/ɪt/ /ɪz/ /'dɪfɪkəlt/ /tə/ /'fɪnɪʃ/ /ðə/ /wɝk/"},{zh:"一天",en:"a day",kk:"/ə/ /de/"},{zh:"在一天内",en:"in a day",kk:"/ɪn/ /ə/ /de/"},{zh:`在一天内完成这个工作
+（它）是困难的`,en:`it is difficult to finish the work in
+a day`,kk:`/ɪt/ /ɪz/ /'dɪfɪkəlt/ /tə/ /'fɪnɪʃ/ /ðə/ /wɝk/
+/ɪn/ /ə/ /de/`},{zh:"危险的",en:"dangerous",kk:"/'dendʒərəs/"},{zh:"它是危险的",en:"it is dangerous",kk:"/ɪt/ /ɪz/ /'dendʒərəs/"},{zh:"游泳",en:"to swim",kk:"/tə/ /swɪm/"},{zh:"大海",en:"the sea",kk:"/ðə/ /si/"},{zh:"在大海里",en:"in the sea",kk:"/ɪn/ /ðə/ /si/"},{zh:"在大海里游泳",en:"to swim in the sea",kk:"/tə/ /swɪm/ /ɪn/ /ðə/ /si/"},{zh:`在大海里游泳（它）是危险
+的`,en:"it is dangerous to swim in the sea",kk:`/ɪt/ /ɪz/ /'dendʒərəs/ /tə/ /swɪm/ /ɪn/ /ðə/
+/si/`},{zh:"认为",en:"think",kk:"/θɪŋk/"},{zh:"我认为",en:"I think",kk:"/aɪ/ /θɪŋk/"},{zh:`我认为在大海里游泳（它）
+是危险的`,en:`I think it is dangerous to swim in
+the sea`,kk:`/aɪ/ /θɪŋk/ /ɪt/ /ɪz/ /'dendʒərəs/ /tə/ /swɪm/
+/ɪn/ /ðə/ /si/`},{zh:`我不认为在大海里游泳
+（它）是危险的`,en:`I don't think it is dangerous to
+swim in the sea`,kk:`/aɪ/ /dont/ /θɪŋk/ /ɪt/ /ɪz/ /'dendʒərəs/ /tə/
+/swɪm/ /ɪn/ /ðə/ /si/`},{zh:"我拒绝",en:"I refuse",kk:"/aɪ/ /ri'fjuz/"},{zh:"我拒绝在大海里游泳",en:"I refuse to swim in the sea",kk:"/aɪ/ /ri'fjuz/ /tə/ /swɪm/ /ɪn/ /ðə/ /si/"},{zh:"因为",en:"because",kk:"/bɪ'kɔz/"},{zh:"因为它是危险的",en:"because it is dangerous",kk:"/bɪ'kɔz/ /ɪt/ /ɪz/ /'dendʒərəs/"},{zh:`我拒绝在大海里游泳因为它
+是危险的`,en:`I refuse to swim in the sea
+because it is dangerous`,kk:`/aɪ/ /ri'fjuz/ /tə/ /swɪm/ /ɪn/ /ðə/ /si/
+/bɪ'kɔz/ /ɪt/ /ɪz/ /'dendʒərəs/`},{zh:"接受",en:"to accept",kk:"/tə/ /ək'sɛpt/"},{zh:"这个挑战",en:"the challenge",kk:"/ðə/ /'tʃælɪndʒ/"},{zh:"接受这个挑战",en:"to accept the challenge",kk:"/tə/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:"我必须接受这个挑战",en:"I have to accept the challenge",kk:"/aɪ/ /hæv/ /tə/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:`我（过去）必须接受这个挑
+战`,en:"I had to accept the challenge",kk:"/aɪ/ /hæd/ /tə/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:`我（过去）不必接受这个挑
+战`,en:`I didn't have to accept the
+challenge`,kk:`/aɪ/ /ˈdɪdnt/ /hæv/ /tə/ /ək'sɛpt/ /ðə/
+/'tʃælɪndʒ/`},{zh:"我接受这个挑战",en:"I accept the challenge",kk:"/aɪ/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:"我不接受这个挑战",en:"I don't accept the challenge",kk:"/aɪ/ /dont/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:"我（过去）接受这个挑战",en:"I accepted the challenge",kk:"/aɪ/ /ək'sɛptɪd/ /ðə/ /'tʃælɪndʒ/"},{zh:"我（过去）不接受这个挑战",en:"I didn't accept the challenge",kk:"/aɪ/ /ˈdɪdnt/ /ək'sɛpt/ /ðə/ /'tʃælɪndʒ/"},{zh:"大的",en:"big",kk:"/bɪɡ/"},{zh:"一个大的挑战",en:"a big challenge",kk:"/ə/ /bɪɡ/ /'tʃælɪndʒ/"},{zh:"英语",en:"English",kk:"/ˈɪŋɡlɪʃ/"},{zh:"英语是一个大的挑战",en:"English is a big challenge",kk:"/ˈɪŋɡlɪʃ/ /ɪz/ /ə/ /bɪɡ/ /'tʃælɪndʒ/"},{zh:"对我来说",en:"for me",kk:"/fɝ/ /mi/"},{zh:`英语是一个大的挑战对我来
+说`,en:"English is a big challenge for me",kk:"/ˈɪŋɡlɪʃ/ /ɪz/ /ə/ /bɪɡ/ /'tʃælɪndʒ/ /fɝ/ /mi/"},{zh:`英语不是一个大的挑战对我
+来说`,en:`English is not a big challenge for
+me`,kk:`/ˈɪŋɡlɪʃ/ /ɪz/ /nɑt/ /ə/ /bɪɡ/ /'tʃælɪndʒ/ /fɝ/
+/mi/`},{zh:"掌握",en:"to master",kk:"/tə/ /'mæstɚ/"},{zh:"英语",en:"English",kk:"/ˈɪŋɡlɪʃ/"},{zh:"掌握英语",en:"to master English",kk:"/tə/ /'mæstɚ/ /ˈɪŋɡlɪʃ/"},{zh:"我将会",en:"I will",kk:"/aɪ/ /wɪl/"},{zh:"我将会掌握英语",en:"I will master English",kk:"/aɪ/ /wɪl/ /'mæstɚ/ /ˈɪŋɡlɪʃ/"},{zh:"将来",en:"the future",kk:"/ðə/ /'fjʊtʃɚ/"},{zh:"在将来",en:"in the future",kk:"/ɪn/ /ðə/ /'fjʊtʃɚ/"},{zh:"不远的",en:"near",kk:"/nɪr/"},{zh:"在不远的将来",en:"in the near future",kk:"/ɪn/ /ðə/ /nɪr/ /'fjʊtʃɚ/"},{zh:`我将会在不远的将来掌握英
+语`,en:`I will master English in the near
+future`,kk:`/aɪ/ /wɪl/ /'mæstɚ/ /ˈɪŋɡlɪʃ/ /ɪn/ /ðə/ /nɪr/
+/'fjʊtʃɚ/`},{zh:"我知道",en:"I know",kk:"/aɪ/ /no/"},{zh:`我知道我将会在不远的将来
+掌握英语`,en:`I know I will master English in
+the near future`,kk:`/aɪ/ /no/ /aɪ/ /wɪl/ /'mæstɚ/ /ˈɪŋɡlɪʃ/ /ɪn/
+/ðə/ /nɪr/ /'fjʊtʃɚ/`},{zh:"而且",en:"and",kk:"/ənd/"},{zh:"而且我知道我将会在不远的",en:"and I know I will master english",kk:"/ənd/ /aɪ/ /no/ /aɪ/ /wɪl/ /'mæstɚ/ /ˈɪŋɡlɪʃ/"},{zh:"将来掌握英语",en:"in the near future",kk:"/ɪn/ /ðə/ /nɪr/ /'fjʊtʃɚ/"},{zh:`英语不是一个大的挑战对我
+来说`,en:`English is not a big challenge for
+me`,kk:`/ˈɪŋɡlɪʃ/ /ɪz/ /nɑt/ /ə/ /bɪɡ/ /'tʃælɪndʒ/ /fɝ/
+/mi/`},{zh:`英语不是一个大的挑战对我
+来说而且我知道我将会在不
+远的将来掌握英语`,en:`English is not a big challenge for
+me and I know I will master
+english in the near future`,kk:`/ˈɪŋɡlɪʃ/ /ɪz/ /nɑt/ /ə/ /bɪɡ/ /'tʃælɪndʒ/ /fɝ/
+/mi/ /ənd/ /aɪ/ /no/ /aɪ/ /wɪl/ /'mæstɚ/
+/ˈɪŋɡlɪʃ/ /ɪn/ /ðə/ /nɪr/ /'fjʊtʃɚ/`}];return(w,c)=>{const e=n("StatementGroup");return a(),k("div",null,[r,o,s(e,{data:d}),l])}}});export{m as __pageData,p as default};
